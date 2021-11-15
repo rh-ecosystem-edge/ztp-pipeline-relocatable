5e8246b0b64acdc37b091312fdf2519dbdc5b1d7
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-11-15 16:20:57 +0100

    add preflight to pipeline fix: N/A Add the next verification: 1-. clients installed 2-. Kubeconfig path ok 3-. ocp installed (nodes available) 4-. cluster operators available 5-. pods metal3 available
    Signed-off-by: Alberto Morgante

--- shared-utils/requirements.sh
+++ shared-utils/requirements.sh
@@ -5,6 +5,10 @@ set -o pipefail
 set -o nounset
 set -m
 
+###### Variables
+################
+# OC_KUBECONFIG_PATH=/root/admin.kubeconfig   # change or get from gh-env vars 
+
 echo ">>>> Download jq, oc, kubectl and set bash completion"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 curl -Ls https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 >/usr/bin/jq
@@ -26,6 +30,35 @@ curl -L https://storage.googleapis.com/kubernetes-release/release/$(curl -s http
 chmod u+x /usr/bin/kubectl
 
 oc completion bash >>/etc/bash_completion.d/oc_completion
+
+echo ">>>> Loading the Kubeconfig file"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+if [ ! -f "$OC_KUBECONFIG_PATH" ]; then
+    echo "Error: Kubeconfig file not found in the path passed in github actions"
+    exit 1 
+fi
+export KUBECONFIG="$OC_KUBECONFIG_PATH"
+
+echo ">>>> Verify oc get nodes"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>"
+if [[ $(oc get nodes | grep -i ready | wc -l) -ne 1 ]] && [[ $(oc get nodes | grep -i ready | wc -l) -ne 3 ]]; then
+	echo "Error: Nodes are not ready"
+    exit 2
+fi
+
+echo ">>>> Verify the cluster operator ready"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+if [[ $(oc get co | awk '{print $3}' | grep -i true | wc -l) -ne $(($(oc get co | wc -l) - 1)) ]]; then
+	echo "Error: some cluster operators are not ready"
+    exit 3
+fi
+
+echo ">>>> Verify the metal3 pods ready"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+if [[ $(oc get pod -n openshift-machine-api | wc -l) -lt 1 ]]; then
+	echo "Error: metal3 pods are not available to use ztp"
+    exit 4
+fi
 
 echo ">>>> EOF"
 echo ">>>>>>>>"
