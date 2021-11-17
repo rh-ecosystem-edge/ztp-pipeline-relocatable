f74b2f876c2780c02b0b5b1d8e22e992ce720cfa
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-11-17 17:08:11 +0100

    fix expersion sed error

--- deploy-httpd-server/deploy.sh
+++ deploy-httpd-server/deploy.sh
@@ -14,7 +14,7 @@ echo ">>>> Create httpd server manifest"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 domain=$(grep server "$KUBECONFIG" | awk '{print $2}' | cut -d '.' -f 2- | cut -d ':' -f 1)
-sed -i "s/CHANGEDOMAIN/apps.$domain/g" http-server.yml
+sed -i "s%CHANGEDOMAIN%apps.$domain%g" http-server.yml
 
 oc create -f http-server.yml
 ../"$SHARED_DIR"/wait_for_deployment.sh -t 1000 -n default nginx
