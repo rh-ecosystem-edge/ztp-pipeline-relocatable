a223d0438c572bedefca9b85d98d37d96be8c7dd
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-11-18 13:10:46 +0100

    fixed error with registry
    Signed-off-by: Alberto Morgante

--- deploy-disconnected-registry/deploy.sh
+++ deploy-disconnected-registry/deploy.sh
@@ -19,6 +19,7 @@ echo ">>>> Get the pull secret from hub to file ./pull-secret.json"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 oc get secret -n openshift-config pull-secret -ojsonpath='{.data.\.dockerconfigjson}' | base64 -d > ./pull-secret.json
+PULL_SECRET=./pull-secret.json
 
 echo ">>>> Get the registry cert and update pull secret"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
@@ -28,27 +29,19 @@ LOCAL_REG=$(oc get route -n openshift-image-registry | awk '{print $2}' | tail -
 oc get secret -n openshift-ingress  router-certs-default -o go-template='{{index .data "tls.crt"}}' | base64 -d > /etc/pki/ca-trust/source/anchors/internal-registry.crt
 update-ca-trust extract
 
+
 sed -i "s%CHANGE_OCP_RELEASE%$OC_OCP_VERSION%g" olm-mirror.sh 
-sed -i "s%CHANGE_PULLSECRET_FILE%$KUBECONFIG%g" olm-mirror.sh
+sed -i "s%CHANGE_PULLSECRET_FILE%$PULL_SECRET%g" olm-mirror.sh
 sed -i "s%CHANGE_ROUTE_REGISTRY%$LOCAL_REG%g" olm-mirror.sh
 
-
+#TODO: change user to avoid request the kubeadmin password
 oc login -u kubeadmin -p $OC_KUBEADMIN_PASS_SECRET
-TOKEN=$(oc whoami -t)
-oc logout ; oc config use-context admin
-KEY=$( echo -n kubeadmin:"$TOKEN" | base64 -w0)
 export REGISTRY_NAME="$(oc get route -n openshift-image-registry default-route -o jsonpath={'.status.ingress[0].host'})"
-jq ".auths += {\"$REGISTRY_NAME\": {\"auth\": \"$KEY\",\"email\": \"info@alklabs.com\"}}" < ./pull-secret.json  > ./pull-secret-internal-registry.json
+podman login $REGISTRY_NAME -u kubeadmin -p $(oc whoami -t) --authfile=./pull-secret-internal-registry.json
+oc logout ; oc config use-context admin
+oc create ns ocp4
 
-
-./olm-mirror.sh mirror
-#./olm-mirror.sh mirror-olm
-
-
-
-
-
-#oc adm release mirror -a ./pull-secret-internal-registry.json --from="$OPENSHIFT_RELEASE_IMAGE" --to-release-image="${LOCAL_REG}"/ocp4/openshift4:"${OCP_RELEASE}" --to="${LOCAL_REG}"/ocp4/openshift4
+oc adm release mirror -a ./pull-secret-internal-registry.json --from="$OPENSHIFT_RELEASE_IMAGE" --to-release-image="${LOCAL_REG}"/ocp4/openshift4:"${OCP_RELEASE}" --to="${LOCAL_REG}"/ocp4/openshift4
 
 echo ">>>>EOF"
 echo ">>>>>>>"
