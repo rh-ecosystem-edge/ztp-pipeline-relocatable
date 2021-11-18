01f8c98deb8f137090b056893c43963e70b3ad16
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-11-18 11:32:04 +0100

    add opm cli

--- shared-utils/verify_preflight.sh
+++ shared-utils/verify_preflight.sh
@@ -27,6 +27,13 @@ rm -rf oc.tar.gz
 mv oc /usr/bin
 chmod +x /usr/bin/oc
 
+cd /root/bin 
+curl -k -s  https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest/opm-linux.tar.gz > opm.tar.gz
+tar zxf opm.tar.gz
+rm -rf opm.tar.gz
+mv opm /usr/bin
+chmod +x /usr/bin/opm
+
 curl -L https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl >/usr/bin/kubectl
 chmod u+x /usr/bin/kubectl
 
