88af61035d586299cff1117eaf5c203f1cba30a6
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-11-22 13:06:45 +0100

    add deploy using kustomize
    Signed-off-by: Alberto Morgante

---
+++ deploy-spoke/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -m
+
+# variables
+# #########
+# uncomment it, change it or get it from gh-env vars (default behaviour: get from gh-env)
+# export KUBECONFIG=/root/admin.kubeconfig
+
+echo ">>>> Deploy all the manifests using kustomize"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+oc apply -k . 
+
+echo ">>>>EOF"
+echo ">>>>>>>"
