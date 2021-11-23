bee1d9c96497babf725a41c630e52df5907a29ab
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2021-11-23 19:17:39 +0100

    Adding revamped mirror process
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- deploy-disconnected-registry/deploy.sh
+++ deploy-disconnected-registry/deploy.sh
@@ -7,41 +7,20 @@ set -m
 
 # variables
 # #########
-# uncomment it, change it or get it from gh-env vars (default behaviour: get from gh-env)echo
+source ./common.sh ${1}
 
 echo ">>>> Enable internal registry"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-
 oc patch configs.imageregistry.operator.openshift.io cluster --type merge --patch '{"spec":{"managementState":"Managed"}}'
 sleep 10
 oc patch configs.imageregistry.operator.openshift.io cluster --type merge --patch '{"spec":{"storage":{"pvc":{"claim":null}}}}'
 sleep 10
 oc patch configs.imageregistry.operator.openshift.io/cluster --type=merge --patch '{"spec":{"defaultRoute":true}}'
 sleep 60
-echo ">>>> Get the pull secret from hub to file ./pull-secret.json"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
-oc get secret -n openshift-config pull-secret -ojsonpath='{.data.\.dockerconfigjson}' | base64 -d >./pull-secret.json
-PULL_SECRET=./pull-secret.json
-
-echo ">>>> Get the registry cert and update pull secret"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-OPENSHIFT_RELEASE_IMAGE=$(oc get clusterversion -o jsonpath={'.items[0].status.desired.image'})
-OCP_RELEASE=$(oc get clusterversion -o jsonpath={'.items[0].status.desired.version'})-x86_64
-LOCAL_REG=$(oc get route -n openshift-image-registry | awk '{print $2}' | tail -1)
-oc get secret -n openshift-ingress router-certs-default -o go-template='{{index .data "tls.crt"}}' | base64 -d >/etc/pki/ca-trust/source/anchors/internal-registry.crt
+echo ">>>> Trusting internal registry"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+## Update trusted CA from Helper
+oc --kubeconfig=${KUBECONFIG_HUB} get secret -n openshift-ingress router-certs-default -o go-template='{{index .data "tls.crt"}}' | base64 -d >/etc/pki/ca-trust/source/anchors/internal-registry.crt
 update-ca-trust extract
 
-if [ $(oc get ns | grep ocp4 | wc -l) -eq 0 ]; then
-	oc create ns ocp4
-fi
-
-export REGISTRY_NAME="$(oc get route -n openshift-image-registry default-route -o jsonpath={'.status.ingress[0].host'})"
-oc -n ocp4 create sa robot
-oc -n ocp4 adm policy add-role-to-user registry-editor -z robot
-podman login $REGISTRY_NAME -u robot -p $(oc -n ocp4 serviceaccounts get-token robot) --authfile=./pull-secret.json
-oc adm release mirror -a ./pull-secret.json --from="$OPENSHIFT_RELEASE_IMAGE" --to-release-image="${LOCAL_REG}"/ocp4/openshift4:"${OCP_RELEASE}" --to="${LOCAL_REG}"/ocp4/openshift4
-#oc logout ; oc config use-context admin
-
-echo ">>>>EOF"
-echo ">>>>>>>"
---
+++ deploy-disconnected-registry/olm-sync.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+function create_cs() {
+		cat > ./catalogsource.yaml <<EOF
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: kubeframe-catalog
+  namespace: openshift-marketplace
+spec:
+  sourceType: grpc
+  image: ${OLM_DESTINATION_INDEX}
+  displayName: Disconnected Lab
+  publisher: disconnected-lab
+  updateStrategy:
+    registryPoll:
+      interval: 30m
+EOF
+
+		echo ""
+		echo "To apply the Red Hat Operators catalog mirror configuration to your cluster, do the following once per cluster:"
+		echo "oc apply -f ./catalogsource.yaml"
+}
+
+function prepare_env() {
+    ## Load Env
+    source ./common.sh ${1}
+
+    ## Checks
+    fail_counter=0
+    for binary in {opm,oc,skopeo,podman}; do
+        if [[ -z $(command -v $binary) ]]; then
+            echo "You need to install $binary!"
+            let "fail_counter++"
+        fi
+    done
+
+    if [[ ! -f ${PULL_SECRET} ]]; then
+        echo ">> Pull Secret not found in ${PULL_SECRET}!"
+        let "fail_counter++"
+    fi
+
+    if [[ ${fail_counter} -ge 1 ]]; then
+        echo "#########"
+        #exit 1
+    fi
+
+    echo ">>>> Creating Namespace and Service Accounts"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    if [ $(oc get ns | grep olm | wc -l) -eq 0 ]; then
+          oc create ns ${DESTINATION_REGISTRY_IMAGE_NS} 
+    fi
+    
+    oc -n ${DESTINATION_REGISTRY_IMAGE_NS} create sa robot
+    oc -n ${DESTINATION_REGISTRY_IMAGE_NS} adm policy add-role-to-user registry-editor -z robot
+
+
+    echo "Source Index: ${SOURCE_INDEX}"
+    echo "Destination Index: ${OLM_DESTINATION_INDEX}"
+    echo "Destination Registry: ${DESTINATION_REGISTRY}"
+}
+
+function mirror() {
+	# Check for credentials for OPM
+	if [ ! -f ~/.docker/config.json ]; then
+		echo "ERROR: missing ~/.docker/config.json config"
+		exit 1
+	fi
+
+    echo ">>>> Mirror OLM Operators"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>"
+	# Mirror redhat-operator index image
+	echo "opm index prune --from-index ${SOURCE_INDEX} --packages ${SOURCE_PACKAGES} --tag ${OLM_DESTINATION_INDEX}"
+	opm index prune --from-index ${SOURCE_INDEX} --packages ${SOURCE_PACKAGES} --tag ${OLM_DESTINATION_INDEX}
+	GODEBUG=x509ignoreCN=0 podman push --tls-verify=false ${OLM_DESTINATION_INDEX} --authfile ${PULL_SECRET}
+	GODEBUG=x509ignoreCN=0 oc adm catalog mirror ${OLM_DESTINATION_INDEX} ${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS} --registry-config=${PULL_SECRET}
+}
+
+prepare_env ${1}
+mirror
+
+exit 0
