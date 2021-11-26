5d8b9a49761cad888bfd0386524c0c24c7da5d47
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-11-26 16:43:15 +0100

    fix error with sed in replacing vars

--- hack/deploy-hub-local/build.sh
+++ hack/deploy-hub-local/build.sh
@@ -50,15 +50,15 @@ else
    kcli create kube openshift --force --paramfile lab-withoutMetal3.yml -P tag="$OC_RELEASE" -P cluster="$OC_CLUSTER_NAME" "$OC_CLUSTER_NAME"
 fi
 
-IP=$(kcli info vm test-ci-installer|grep ip | awk '{print $2}')
-SPOKE1_0=$(kcli info vm spoke1-m0 | grep id | awk '{print $2}')
-SPOKE1_1=$(kcli info vm spoke1-m1 | grep id | awk '{print $2}')
-SPOKE1_2=$(kcli info vm spoke1-m2 | grep id | awk '{print $2}')
+IP=$(kcli info vm test-ci-installer|grep ip|awk '{print $2}')
+SPOKE1_0=$(kcli info vm spoke1-m0|grep id|awk '{print $2}')
+SPOKE1_1=$(kcli info vm spoke1-m1|grep id|awk '{print $2}')
+SPOKE1_2=$(kcli info vm spoke1-m2|grep id|awk '{print $2}')
 
-sed  "s/CHANGE_IP/$IP/g" ./spokes-orig.yaml > ./spokes.yaml
-sed  "s/CHANGE_ID_0/$SPOKE1_0/g" ./spokes-orig.yaml > ./spokes.yaml
-sed  "s/CHANGE_ID_1/$SPOKE1_1/g" ./spokes-orig.yaml > ./spokes.yaml
-sed  "s/CHANGE_ID_2/$SPOKE1_2/g" ./spokes-orig.yaml > ./spokes.yaml
+sed "s/CHANGE_IP/$IP/g" ./spokes-orig.yaml > ./spokes.yaml
+sed "s/CHANGE_ID_0/$SPOKE1_0/g" ./spokes-orig.yaml >> ./spokes.yaml
+sed "s/CHANGE_ID_1/$SPOKE1_1/g" ./spokes-orig.yaml >> ./spokes.yaml
+sed "s/CHANGE_ID_2/$SPOKE1_2/g" ./spokes-orig.yaml >> ./spokes.yaml
 
 kcli create dns -n bare-net httpd-server.apps.test-ci.alklabs.com -i 192.168.150.252
 
