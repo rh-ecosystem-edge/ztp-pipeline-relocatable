e9c4bdf6e7f93006f9ca1d201d76ad20f3064428
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-11-27 22:36:28 +0100

    first version for spoke validation using matrix steps
    Signed-off-by: Alberto Morgante

--- deploy-spoke/wait_for_spoke.sh
+++ deploy-spoke/wait_for_spoke.sh
@@ -5,33 +5,53 @@ set -o pipefail
 set -o nounset
 set -m
 
-# variables
-# #########
-SPOKE="$1"
-wait_time=3600
 
-echo ">>>> Wait for bmh of spoke $SPOKE"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+function validate_condition(){ 
+  timeout=0
+  ready=false
+  while [ "$timeout" -lt "$wait_time" ] ; do  
+      test "$(oc get $1 -o jsonpath=$2)" == "${states_machine[$1,$2]}" && ready=true && break;
+      echo "Waiting for spoke cluster $SPOKE to be deployed"
+      sleep 60
+      timeout=$(($timeout + 1))
+  done
+
+ if [ "$ready" == "false" ] ; then
+    echo "timeout waiting for spoke cluster $SPOKE to be deployed"
+    exit 1
+  else
+    echo "Condition $1 verified"
+  fi  
+}
 
 
+## main function
+##
 
-echo ">>>> Wait for deployment of spoke $SPOKE"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-
-timeout=0
-ready=false
-while [ "$timeout" -lt "$wait_time" ] ; do
-  SPOKE_STATUS=$(oc get -n $SPOKE AgentClusterInstall $SPOKE -o jsonpath='{.status.debugInfo.state}')
-  test "$SPOKE_STATUS" == "installed" && ready=true && break;
-  echo "Waiting for spoke cluster $SPOKE to be deployed"
-  sleep 60
-  timeout=$(($timeout + 5))
-done
-if [ "$ready" == "false" ] ; then
- echo timeout waiting for spoke cluster $SPOKE to be deployed
- exit 1
-else
- echo cluster $SPOKE deployed
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <spoke>"
+  exit 1
 fi
+
+## variables
+## #########
+SPOKE="$1"
+wait_time=60   # wait until 60 min
+declare -A states_machine
+states_machine['bmh','{.items[*].status.errorCount}']='0 0 0'  # bmh's without errors
+states_machine['bmh','{.items[*].status.provisioning.state}']='provisioned provisioned provisioned'  # bmh's state provisioned
+states_machine['agent','{.items[*].spec.approved}']='true true true'  # agent's state approved
+states_machine['agentclusterinstall','{.items[*].status.debugInfo.stateInfo}']='Cluster is installed' # agent cluster install state
+
+
+echo ">>>> Starting the validation until finish the installation"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+oc project $SPOKE
+validate_condition "bmh" "{.items[*].status.errorCount}"
+validate_condition "bmh" "{.items[*].status.provisioning.state}"
+validate_condition "agent" "{.items[*].spec.approved}"
+validate_condition "agentclusterinstall" "{.items[*].status.debugInfo.stateInfo}"
+
+
 echo ">>>>EOF"
 echo ">>>>>>>"
