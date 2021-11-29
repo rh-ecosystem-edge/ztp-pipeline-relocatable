f3fef02ecdb96ce116786708dc5c987eacf1384b
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2021-11-29 15:51:41 +0100

    updating the NS of deploy-httpd
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- deploy-httpd-server/deploy.sh
+++ deploy-httpd-server/deploy.sh
@@ -12,16 +12,17 @@ set -m
 
 # Load common vars
 source ${WORKDIR}/shared-utils/common.sh
+export HTTPD_NS=default
 
 echo ">>>> Create httpd server manifest"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
-domain=$(oc get ingresscontroller -n openshift-ingress-operator default -o jsonpath='{.status.domain}')
+domain=$(oc get ingresscontroller -n openshift-ingress-operator ${HTTPD_NS} -o jsonpath='{.status.domain}')
 sed -i "s%CHANGEDOMAIN%$domain%g" http-server.yml
 
 # TODO: create on their proper NS
-oc apply -n default -f http-server.yml
-../"$SHARED_DIR"/wait_for_deployment.sh -t 1000 -n default nginx
+oc apply -n ${HTTPD_NS} -f http-server.yml
+../"$SHARED_DIR"/wait_for_deployment.sh -t 1000 -n ${HTTPD_NS} nginx
 
 echo ">>>> Pre-load the images rhcos to be available"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
@@ -30,10 +31,10 @@ RHCOS_ISO="https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/pre-
 RHCOS_ROOTFS="https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/pre-release/latest/rhcos-live-rootfs.x86_64.img"
 BASE_ISO=$(basename $RHCOS_ISO)
 BASE_ROOTFS=$(basename $RHCOS_ROOTFS)
-podname=$(oc get pod -n default | grep nginx | awk '{print $1}')
+podname=$(oc get pod -n ${HTTPD_NS} | grep nginx | awk '{print $1}')
 
-oc exec $podname -- curl -Lk $RHCOS_ISO -o /usr/share/nginx/html/"$BASE_ISO"
-oc exec $podname -- curl -Lk $RHCOS_ROOTFS -o /usr/share/nginx/html/"$BASE_ROOTFS"
+oc exec -n ${HTTPD_NS} $podname -- curl -Lk $RHCOS_ISO -o /usr/share/nginx/html/"$BASE_ISO"
+oc exec -n ${HTTPD_NS} $podname -- curl -Lk $RHCOS_ROOTFS -o /usr/share/nginx/html/"$BASE_ROOTFS"
 
 echo ">>>>EOF"
 echo ">>>>>>>"
