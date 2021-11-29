57e8629e2f92c43c1579b889066c5f963914c30a
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-11-29 18:08:56 +0100

    add feature for verify Signed-off-by: Alberto Morgante

--- deploy-acm/deploy.sh
+++ deploy-acm/deploy.sh
@@ -10,44 +10,49 @@ set -m
 # uncomment it, change it or get it from gh-env vars (default behaviour: get from gh-env)
 # export KUBECONFIG=/root/admin.kubeconfig
 
-# Load common vars
-source ${WORKDIR}/shared-utils/common.sh
+if ./verify.sh; then
 
-echo ">>>> Modify files to replace with pipeline info gathered"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-sed -i "s/CHANGEME/$OC_ACM_VERSION/g" 03-subscription.yml
+	# Load common vars
+	source ${WORKDIR}/shared-utils/common.sh
 
-echo ">>>> Deploy manifests to install ACM $OC_ACM_VERSION"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-oc apply -f 01-namespace.yml -o yaml --dry-run=client | oc apply -f -
-sleep 2
-oc apply -f 02-operatorgroup.yml 
-sleep 2
-oc apply -f 03-subscription.yml
-sleep 60
+	echo ">>>> Modify files to replace with pipeline info gathered"
+	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+	sed -i "s/CHANGEME/$OC_ACM_VERSION/g" 03-subscription.yml
 
-echo ">>>> Deploy ACM cr manifest"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
-oc apply -f 04-acm-cr.yml
-sleep 60
+	echo ">>>> Deploy manifests to install ACM $OC_ACM_VERSION"
+	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+	oc apply -f 01-namespace.yml -o yaml --dry-run=client | oc apply -f -
+	sleep 2
+	oc apply -f 02-operatorgroup.yml 
+	sleep 2
+	oc apply -f 03-subscription.yml
+	sleep 60
 
-echo ">>>> Wait until acm ready"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>"
-timeout=0
-ready=false
-sleep 240
-while [ "$timeout" -lt "60" ]; do
-	if [[ $(oc get pod -n open-cluster-management | grep -i running | wc -l) -eq $(oc get pod -n open-cluster-management | grep -v NAME | wc -l) ]]; then
-		ready=true
-		break
+	echo ">>>> Deploy ACM cr manifest"
+	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
+	oc apply -f 04-acm-cr.yml
+	sleep 60
+
+	echo ">>>> Wait until acm ready"
+	echo ">>>>>>>>>>>>>>>>>>>>>>>>>"
+	timeout=0
+	ready=false
+	sleep 240
+	while [ "$timeout" -lt "60" ]; do
+		if [[ $(oc get pod -n open-cluster-management | grep -i running | wc -l) -eq $(oc get pod -n open-cluster-management | grep -v NAME | wc -l) ]]; then
+			ready=true
+			break
+		fi
+		sleep 5
+		timeout=$((timeout + 5))
+	done
+	if [ "$ready" == "false" ]; then
+		echo "timeout waiting for ACM pods "
+		exit 1
 	fi
-	sleep 5
-	timeout=$((timeout + 5))
-done
-if [ "$ready" == "false" ]; then
-	echo "timeout waiting for ACM pods "
-	exit 1
+elif [[ $? -eq 50 ]]; then
+	echo ">>>> Verify failed...Some pods are failing..." #TODO change to remove and launch again  
+	exit 50
 fi
-
 echo ">>>>EOF"
 echo ">>>>>>>"
---
+++ deploy-acm/verify.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -m
+
+# variables
+# #########
+# uncomment it, change it or get it from gh-env vars (default behaviour: get from gh-env)
+# export KUBECONFIG=/root/admin.kubeconfig
+
+# Load common vars
+source ${WORKDIR}/shared-utils/common.sh
+
+if [[ $(oc get ns | grep open-cluster-management | wc -l) -eq 0 ]]; then
+  #Open-Cluster-Management namespace does not exist. Launching the step to create it...
+  exit 0
+elif [[ $(oc get pod -n open-cluster-management | grep -i running | wc -l) -eq $(oc get pod -n open-cluster-management | grep -v NAME | wc -l) ]]; then
+  #All pods for ACM running...Skipping the step to create it
+  exit 1
+else
+  #Some pods are failing...Stop pipe to solve it  #TODO this scenario we should remove the subscription and destroy everything and relaunch again
+  exit 50
+fi 
+
+echo ">>>>EOF"
+echo ">>>>>>>"
