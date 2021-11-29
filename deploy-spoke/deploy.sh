82de14aef26bf36d57b453dc0b5c079c718d1f28
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-11-29 20:12:33 +0100

    fix error with spoke

--- deploy-spoke/deploy.sh
+++ deploy-spoke/deploy.sh
@@ -10,12 +10,14 @@ set -m
 # uncomment it, change it or get it from gh-env vars (default behaviour: get from gh-env)
 # export KUBECONFIG=/root/admin.kubeconfig
 
+# Load common vars
+source ${WORKDIR}/shared-utils/common.sh
+
 echo ">>>> Deploy all the manifests using kustomize"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 oc patch provisioning provisioning-configuration --type merge -p '{"spec":{"watchAllNamespaces": true}}'
-# Load common vars
-source ${WORKDIR}/shared-utils/common.sh
+
 
 cd ${OUTPUTDIR}
 oc apply -f ./
