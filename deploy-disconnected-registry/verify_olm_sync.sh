537d674690e4194f4095200fa97b7540ea70ebfd
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-11-30 13:52:17 +0100

    add basic verification ocp and olm

--- deploy-disconnected-registry/verify_olm_sync.sh
+++ deploy-disconnected-registry/verify_olm_sync.sh
@@ -14,20 +14,31 @@ set -m
 source ${WORKDIR}/shared-utils/common.sh
 source ./common.sh hub
 
-podman login ${DESTINATION_REGISTRY} -u ${REG_US} -p ${REG_PASS} --authfile=${PULL_SECRET}
-for packagemanifest in $(oc get packagemanifest -n openshift-marketplace -o name ${PACKAGES_FORMATED}); do
-	for package in $(oc get $packagemanifest -o jsonpath='{.status.channels[*].currentCSVDesc.relatedImages}' | sed "s/ /\n/g" | tr -d '[],' | sed 's/"/ /g'); do
-		echo
-		echo "Package: ${package}"
-		skopeo inspect docker://${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS}/$(echo $package | awk -F'/' '{print $2}')-$(basename $package) --authfile ${PULL_SECRET}
-		if [ $? -eq 0 ]; then
-    #In this case, we don't need to mirror catalogs, everything is already there
-      break;
-		else
-    #echo "Must mirror catalog...Launching step to create them"
-      exit 0
-    fi
-	done
-done
-#In this case, we don't need to mirror catalogs, everything is already there
-exit 1
+## TODO improve with this try
+#podman login ${DESTINATION_REGISTRY} -u ${REG_US} -p ${REG_PASS} --authfile=${PULL_SECRET}
+#for packagemanifest in $(oc get packagemanifest -n openshift-marketplace -o name ${PACKAGES_FORMATED}); do
+#	for package in $(oc get $packagemanifest -o jsonpath='{.status.channels[*].currentCSVDesc.relatedImages}' | sed "s/ /\n/g" | tr -d '[],' | sed 's/"/ /g'); do
+#		echo
+#		echo "Package: ${package}"
+#		skopeo inspect docker://${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS}/$(echo $package | awk -F'/' '{print $2}')-$(basename $package) --authfile ${PULL_SECRET}
+#		if [ $? -eq 0 ]; then
+#    #In this case, we don't need to mirror catalogs, everything is already there
+#      break;
+#		else
+#    #echo "Must mirror catalog...Launching step to create them"
+#      exit 0
+#    fi
+#	done
+#done
+##In this case, we don't need to mirror catalogs, everything is already there
+#exit 1
+if [[ $(oc get ns | grep ${REGISTRY} | wc -l) -eq 0 ]]; then
+	#namespace does not exist. Launching the step to create it...
+	exit 0
+elif [[ $(oc get -n kubeframe-registry deployment kubeframe-registry -ojsonpath='{.status.availableReplicas}') -eq 0 ]]; then
+	#Resources are not ready...Launching the step to create them...
+	exit 0
+else
+	#Everyting is ready
+	exit 1
+fi
