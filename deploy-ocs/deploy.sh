bd39f114ca4d8b5942d8711348de43b2274578ed
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2021-12-01 18:30:50 +0100

    Added huge deploy.sh including render
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- deploy-ocs/deploy.sh
+++ deploy-ocs/deploy.sh
@@ -5,8 +5,45 @@ set -o pipefail
 set -o nounset
 set -m
 
-# variables
-# #########
+function render_file() {
+    SOURCE_FILE=${1}
+    if [[ $# -lt 1 ]]; then
+        echo "Usage :"
+        echo "  $0 <SOURCE FILE> <(optional) DESTINATION_FILE>"
+        exit 1
+    fi
+
+    DESTINATION_FILE=${2:-""}
+    if [[ "${DESTINATION_FILE}" == "" ]];then
+        envsubst < ${SOURCE_FILE} | oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f -  
+    else
+        envsubst < ${SOURCE_FILE} > ${DESTINATION_FILE}
+    fi
+}
+
+function extract_vars() {
+    # Extract variables from config file
+    DISKS_PATH=${1}
+    raw_disks=$(yq eval "${DISKS_PATH}" "${SPOKES_FILE}" | sed s/null//)                   
+    disks=$(echo ${raw_disks}| tr -d '\ '| sed s#-#,/dev/#g | sed 's/,*//'  | sed 's/,*//')
+    
+    for node in $(oc get nodes -o name | sed s#node\/##)
+    do 
+        nodes+="${node},"
+    done
+    
+    nodes=$(echo ${nodes%*,})
+    
+    # Final Variables
+    export CHANGEME_NODES="[${nodes}]"  
+    export CHANGEME_DEVICES="[${disks}]"
+}
+
+function extract_kubeconfig() {
+    ## Extract the Spoke kubeconfig and put it on the shared folder
+    export SPOKE_KUBECONFIG=${OUTPUTDIR}/kubeconfig-${1}
+    oc --kubeconfig=${HUB_KUBECONFIG} get secret -n $spoke $spoke-admin-kubeconfig -o jsonpath=‘{.data.kubeconfig}’ | base64 -d > ${SPOKE_KUBECONFIG}
+}
 
 # Load common vars
 source ${WORKDIR}/shared-utils/common.sh
@@ -14,83 +51,90 @@ source ${WORKDIR}/shared-utils/common.sh
 echo ">>>> Modify files to replace with pipeline info gathered"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 sed -i "s/CHANGEME/$OC_OCS_VERSION/g" manifests/03-OCS-Subscription.yaml
-
-echo ">>>> Deploy manifests to install LSO"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-oc apply -f 01-LSO-Namespace.yaml
-sleep 2
-oc apply -f 02-LSO-OperatorGroup.yaml 
-sleep 2
-oc apply -f 03-LSO-Subscription.yaml
-sleep 20
-
-echo ">>>> Deploying LSO LocalVolume"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-oc apply -f 04-LSO-LocalVolume.yaml
-sleep 30
-
-echo ">>>> Waiting for: LSO PVs"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>"
-timeout=0
-ready=false
-while [ "$timeout" -lt "60" ]; do
-	if [[ $(oc get pv -o name | wc -l) -eq 3 ]]; then
-		ready=true
-		break
-	fi
-	sleep 5
-	timeout=$((timeout + 5))
-done
-
-if [ "$ready" == "false" ]; then
-	echo "timeout waiting for LSO PVs..."
-	exit 1
+    
+if [[ -z "${ALLSPOKES}" ]]; then
+    ALLSPOKES=$(yq e '(.spokes[] | keys)[]' ${SPOKES_FILE})
 fi
-
-echo ">>>> Deploy manifests to install OCS $OC_OCS_VERSION"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-oc apply -f 01-OCS-Namespace.yaml
-sleep 2
-oc apply -f 02-OCS-OperatorGroup.yaml
-sleep 2
-oc apply -f 03-OCS-Subscription.yaml
-sleep 60
-
-echo ">>>> Labeling nodes for OCS"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
-counter=0
-for node in $(oc get node -o name);
+ 
+for spoke in ${ALLSPOKES}
 do
-    oc label $node cluster.ocs.openshift.io/openshift-storage=''
-    oc label $node topology.rook.io/rack=rack${counter}
-    let "counter+=1" 
+    echo ">>>> Deploy manifests to install LSO and LocalVolume: ${spoke}"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    echo "Extract Kubeconfig for ${spoke}"
+    extract_kubeconfig ${spoke}
+    echo "Filling vars for ${spoke}"
+    extract_vars ".spokes[].${spoke}.master0.storage_disk" 
+    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/01-LSO-Namespace.yaml
+    sleep 2
+    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/02-LSO-OperatorGroup.yaml 
+    sleep 2
+    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/03-LSO-Subscription.yaml
+    sleep 20
+    render_file manifests/04-LSO-LocalVolume.yaml
+    sleep 20
+
+    echo ">>>> Waiting for: LSO PVs on ${spoke}"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    timeout=0
+    ready=false
+    while [ "$timeout" -lt "60" ]; do
+    	if [[ $(oc --kubeconfig=${SPOKE_KUBECONFIG} get pv -o name | wc -l) -eq 3 ]]; then
+    		ready=true
+    		break
+    	fi
+    	sleep 5
+    	timeout=$((timeout + 5))
+    done
+    
+    if [ "$ready" == "false" ]; then
+    	echo "timeout waiting for LSO PVs..."
+    	exit 1
+    fi
+
+    echo ">>>> Deploy manifests to install OCS $OC_OCS_VERSION"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/01-OCS-Namespace.yaml
+    sleep 2
+    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/02-OCS-OperatorGroup.yaml
+    sleep 2
+    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/03-OCS-Subscription.yaml
+    sleep 60
+    
+    echo ">>>> Labeling nodes for OCS"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    counter=0
+    for node in $(oc get node -o name);
+    do
+        oc --kubeconfig=${SPOKE_KUBECONFIG} label $node cluster.ocs.openshift.io/openshift-storage=''
+        oc --kubeconfig=${SPOKE_KUBECONFIG} label $node topology.rook.io/rack=rack${counter}
+        let "counter+=1" 
+    done
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    
+    
+    echo ">>>> Deploy OCS StorageCluster"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/04-OCS-StorageCluster.yaml
+    sleep 60
+    
+    echo ">>>> Waiting for: OCS Cluster"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    timeout=0
+    ready=false
+    sleep 240
+    while [ "$timeout" -lt "60" ]; do
+    	if [[ $(oc get --kubeconfig=${SPOKE_KUBECONFIG} pod -n openshift-storage | grep -i running | wc -l) -eq $(oc --kubeconfig=${SPOKE_KUBECONFIG} get pod -n openshift-storage | grep -v NAME | wc -l) ]]; then
+    		ready=true
+    		break
+    	fi
+    	sleep 5
+    	timeout=$((timeout + 5))
+    done
+    if [ "$ready" == "false" ]; then
+    	echo "timeout waiting for OCS pods..."
+    	exit 1
+    fi
 done
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
-
-
-echo ">>>> Deploy OCS StorageCluster"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
-oc apply -f 04-OCS-StorageCluster.yaml
-sleep 60
-
-echo ">>>> Waiting for: OCS Cluster"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-timeout=0
-ready=false
-sleep 240
-while [ "$timeout" -lt "60" ]; do
-	if [[ $(oc get pod -n openshift-storage | grep -i running | wc -l) -eq $(oc get pod -n openshift-storage | grep -v NAME | wc -l) ]]; then
-		ready=true
-		break
-	fi
-	sleep 5
-	timeout=$((timeout + 5))
-done
-if [ "$ready" == "false" ]; then
-	echo "timeout waiting for OCS pods..."
-	exit 1
-fi
 
 echo ">>>>EOF"
 echo ">>>>>>>"
-
