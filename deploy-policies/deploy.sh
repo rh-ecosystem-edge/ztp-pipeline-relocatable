16bd652e30cb1c4b01a31065f6d784841cdfcf9a
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2021-12-02 15:56:44 +0100

    adding deploy skeleton, does not work
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

---
+++ deploy-policies/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -m
+
+# variables
+# #########
+# uncomment it, change it or get it from gh-env vars (default behaviour: get from gh-env)
+# export KUBECONFIG=/root/admin.kubeconfig
+
+# Load common vars
+source ${WORKDIR}/shared-utils/common.sh
+
+echo ">>>> Deploy all the manifests using kustomize"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+
+oc apply -k .
+
+echo ">>>>EOF"
+echo ">>>>>>>"
