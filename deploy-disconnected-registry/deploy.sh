85b9e527195fee35482344cb214811f628a0c45a
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-12-14 15:45:11 +0100

    fix naming

--- deploy-disconnected-registry/deploy.sh
+++ deploy-disconnected-registry/deploy.sh
@@ -98,7 +98,7 @@ if [[ ${MODE} == 'hub' ]]; then
         render_file manifests/machine-config-certs.yaml ${MODE}
         # after machine config is applied, we need to wait for the registry and acm pods and deployments to be ready
         ../"${SHARED_DIR}"/wait_for_deployment.sh -t 1000 -n "${REGISTRY}" "${REGISTRY}"
-        list_acm_deployment=$(oc --kubeconfig=${KUBECONFIG_HUB} get deployment -n open-cluster-management)
+        list_acm_deployment=$(oc --kubeconfig=${KUBECONFIG_HUB} get deployment -n open-cluster-management -o name)
         for i in ${list_acm_deployment}; do
             ../"${SHARED_DIR}"/wait_for_deployment.sh -t 1000 -n open-cluster-management "${i}"
         done
