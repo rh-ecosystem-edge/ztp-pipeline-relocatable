a6d018dbb361c98a81cba5695207df1b8d90e6ed
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-01-18 15:23:49 +0100

    add preflight for dns requirements

--- shared-utils/verify_preflight.sh
+++ shared-utils/verify_preflight.sh
@@ -101,6 +101,16 @@ if [[ $(oc get pv | wc -l) -lt 3 ]]; then
     exit 6
 fi
 
+echo ">>>> Verify the DNS requirements"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+for spoke in ${ALLSPOKES}; do
+  grap_api_ingress ${spoke}
+  if [[ ${SPOKE_API_IP} == "" || ${SPOKE_INGRESS_IP} == "" || ${HUB_BASEDOMAIN} == "" ]] ; then
+    echo "Error: DNS Entry are not available for spoke ${spoke}"
+    exit 7
+  fi
+done
+
 echo ">>>> EOF"
 echo ">>>>>>>>"
 exit 0
