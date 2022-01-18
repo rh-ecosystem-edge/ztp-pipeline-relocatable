7dda01f55cb5be9aa9a40f631a3738baa121440a
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-01-18 16:19:32 +0100

    add echo to variables for dns req

--- shared-utils/verify_preflight.sh
+++ shared-utils/verify_preflight.sh
@@ -105,6 +105,9 @@ echo ">>>> Verify the DNS requirements"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 for spoke in ${ALLSPOKES}; do
   grab_api_ingress ${spoke}
+  echo ">>>> SPOKE_API_IP: ${SPOKE_API_IP}"
+  echo ">>>> SPOKE_API_INGRESS: ${SPOKE_API_INGRESS}"
+  echo ">>>> HUB_BASEDOMAIN: ${HUB_BASEDOMAIN}"
   if [[ ${SPOKE_API_IP} == "" || ${SPOKE_INGRESS_IP} == "" || ${HUB_BASEDOMAIN} == "" ]] ; then
     echo "Error: DNS Entry are not available for spoke ${spoke}"
     exit 7
