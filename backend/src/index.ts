daf25761c09629b65ecca9e1a2c096b34986358c
Marek Libra <marek.libra@gmail.com>
2022-01-28 14:29:52 +0100

    Introduce pre-start checks for ENV variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,20 +5,50 @@ import { liveness, ping, proxy, readiness, serve } from './endpoints';
 
 const PORT = process.env.BACKEND_PORT || 3001;
 
-const app = express();
+const logger = console;
 
-app.get('/ping', ping);
-app.get(`/readinessProbe`, readiness);
-app.get(`/livenessProbe`, liveness);
+const startUpCheck = () => {
+  const requiredEnvVars = [
+    // 'BACKEND_PORT',
+    'FRONTEND_URL',
+    'CLUSTER_API_URL',
+    'TOKEN',
+    'OAUTH2_CLIENT_ID',
+    'OAUTH2_CLIENT_SECRET',
+    'OAUTH2_REDIRECT_URL',
+  ];
+  requiredEnvVars.forEach((env) => {
+    if (!process.env[env]) {
+      logger.error(`Missing required environment variable ${env}, exiting.`);
+      process.exit(1);
+    }
+  });
 
-app.get(`/login`, login);
-app.get(`/login/callback`, loginCallback);
-app.get(`/logout`, logout);
+  if (!process.env.BACKEND_PORT) {
+    console.warn('Optional BACKEND_PORT environment variable is missing, using default: ', PORT)
+  }
+};
 
-app.all(`/api/*`, proxy);
+const start = () => {
+  startUpCheck();
 
-app.get(`/*`, serve);
+  const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+  app.get('/ping', ping);
+  app.get(`/readinessProbe`, readiness);
+  app.get(`/livenessProbe`, liveness);
+
+  app.get(`/login`, login);
+  app.get(`/login/callback`, loginCallback);
+  app.get(`/logout`, logout);
+
+  app.all(`/api/*`, proxy);
+
+  app.get(`/*`, serve);
+
+  app.listen(PORT, () => {
+    logger.info(`Server listening on ${PORT}`);
+  });
+};
+
+start();
