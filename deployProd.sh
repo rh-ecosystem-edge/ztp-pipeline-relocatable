39a47ac9f8e54e3400a70edf49073200dfcbae83
Marek Libra <marek.libra@gmail.com>
2022-02-03 11:52:06 +0100

    deployProd.sh: Add TODOs

--- deployProd.sh
+++ deployProd.sh
@@ -5,8 +5,10 @@ oc new-project kubeframe-ui
 set -ex
 
 cd scripts
+# TODO: make this optional
 ./createTlsSecret.sh
 
+# TODO: parametrize following resources, i.e. for cluster address
 oc apply -f oauthclient.yaml
 oc apply -f deployment.yaml
 oc apply -f service.yaml
