7b775c1987e6231e8a03ffd63f895cf423a0f647
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2022-02-04 12:24:40 +0100

    Fixing the wait step for ocp Pipelines
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- pipelines/bootstrap.sh
+++ pipelines/bootstrap.sh
@@ -5,6 +5,58 @@ set -o nounset
 set -o errexit
 set -m
 
+function check_resource() {
+    # 1 - Resource type: "deployment"
+    # 2 - Resource name: "openshift-pipelines-operator"
+    # 3 - Type Status: "Available"
+    # 4 - Namespace: "openshift-operators"
+
+    if [[ -z "${1}" ]]; then
+        echo "I need a resource to check, value passed: \"${1}\""
+        exit 1
+    fi
+
+    if [[ -z "${2}" ]]; then
+        echo "I need a resource name to check, value passed: \"${2}\""
+        exit 1
+    fi
+
+    if [[ -z "${3}" ]]; then
+        echo "I need a Type Status (E.G 'Available') from status.conditions json field to check, value passed: \"${3}\""
+        exit 1
+    fi
+
+    if [[ -z "${4}" ]]; then
+        echo "I need a Namespace to check the resource into, value passed: \"${4}\""
+        exit 1
+    fi
+
+
+    RESOURCE="${1}"
+    RESOURCE_NAME="${2}"
+    TYPE_STATUS="${3}"
+    NAMESPACE="${4}"
+
+    echo ">>>> Checking Resource: ${RESOURCE} with name ${RESOURCE_NAME}"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    timeout=0
+    ready=false
+    while [ "$timeout" -lt "240" ]; do
+        if [[ $(oc --kubeconfig=${KUBECONFIG_HUB} -n ${NAMESPACE} get ${RESOURCE} ${RESOURCE_NAME} -o jsonpath="{.status.conditions[?(@.type=="${TYPE_STATUS}")].status}") == 'True' ]]; then
+            ready=true
+            break
+        fi
+        echo "Waiting for ${RESOURCE}-${RESOURCE_NAME} to change the status to ${TYPE_STATUS}"
+        sleep 20
+        timeout=$((timeout + 1))
+    done
+
+    if [ "$ready" == "false" ]; then
+        echo "Timeout waiting for ${RESOURCE}-${RESOURCE_NAME} to change the status to ${TYPE_STATUS}"
+        exit 1
+    fi
+}
+
 function create_permissions() {
     echo ">>>> Creating NS ${SPOKE_DEPLOYER_NS} and giving permissions to SA ${SPOKE_DEPLOYER_SA}"
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
@@ -32,24 +84,10 @@ function deploy_openshift_pipelines() {
     echo ">>>> Deploying Openshift Pipelines"
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     oc --kubeconfig=${KUBECONFIG_HUB} apply -f ${PIPELINES_DIR}/manifests/01-subscription.yaml
-    sleep 5
-    
-    echo ">>>> Waiting for: Openshift Pipelines"
-    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    timeout=0
-    ready=false
-    while [ "$timeout" -lt "240" ]; do
-        if [[ $(oc get --kubeconfig=${KUBECONFIG_HUB} pod -n openshift-operators | grep -i running | wc -l) -eq $(oc --kubeconfig=${KUBECONFIG_HUB} get pod -n openshift-operators --no-headers | grep -v Completed | wc -l) ]]; then
-            ready=true
-            break
-        fi
-        sleep 5
-        timeout=$((timeout + 5))
-    done
-    if [ "$ready" == "false" ]; then
-        echo "timeout waiting for Openshift Pipelines pods..."
-        exit 1
-    fi
+    sleep 10
+
+    check_resource "deployment" "openshift-pipelines-operator" "Available" "openshift-operators" 
+    check_resource "deployment" "tekton-operator-webhook" "Available" "openshift-operators" 
 }
 
 
