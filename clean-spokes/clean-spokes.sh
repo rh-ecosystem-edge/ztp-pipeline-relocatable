1a438f48e3bc080fcb63b55d147adeefef900652
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-02-09 09:53:12 +0100

    update clean spokes pipeline
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- clean-spokes/clean-spokes.sh
+++ clean-spokes/clean-spokes.sh
@@ -14,8 +14,8 @@ for SPOKE in ${ALLSPOKES}; do
     echo ">>>> Cleaning the deployed Spokes clusters"
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo Spoke: ${SPOKE}
-    oc delete managedcluster ${SPOKE}
-    oc delete ns ${SPOKE}
+    oc --kubeconfig=${KUBECONFIG_HUB} delete managedcluster ${SPOKE}
+    oc --kubeconfig=${KUBECONFIG_HUB} delete ns ${SPOKE}
     kcli delete vm ${SPOKE}-m0 ${SPOKE}-m1 ${SPOKE}-m2 ${SPOKE}-w0 -y
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 done
