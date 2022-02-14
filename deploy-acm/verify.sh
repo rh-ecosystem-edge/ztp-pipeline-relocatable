0c782b7a2133029de7e265d5bebde3809a569283
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2022-02-14 15:00:47 +0100

    fixing acm verification
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- deploy-acm/verify.sh
+++ deploy-acm/verify.sh
@@ -12,19 +12,11 @@ set -m
 # Load common vars
 source ${WORKDIR}/shared-utils/common.sh
 
-if [[ $(oc get ns | grep open-cluster-management | wc -l) -eq 0 ]]; then
+if [[ $(oc get ns | grep open-cluster-management | wc -l) -eq 0 || $(oc get multiclusterhub -n open-cluster-management --no-headers | wc -l) -eq 0 ]]; then
     #Open-Cluster-Management namespace does not exist. Launching the step to create it...
     exit 0
 elif [[ $(oc get pod -n open-cluster-management | grep -i running | wc -l) -eq $(oc get pod -n open-cluster-management | grep -v NAME | wc -l) ]]; then
     #All pods for ACM running...Skipping the step to create it
-    exit 1
-else
-    #Some pods are failing...Stop pipe to solve it  #TODO this scenario we should remove the subscription and destroy everything and relaunch again
-    exit 50
-fi
-
-if [[ $(oc get multiclusterhub -n open-cluster-management --no-headers | wc -l) -eq 0 ]]; then
-    #Open-Cluster-Management namespace exists but not the MulticlsuterHub, Launching creation step .
     exit 1
 else
     #Some pods are failing...Stop pipe to solve it  #TODO this scenario we should remove the subscription and destroy everything and relaunch again
