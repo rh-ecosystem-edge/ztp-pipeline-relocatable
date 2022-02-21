29911089e112eb592d961005c1894b0b9d6c55c9
Marek Libra <marek.libra@gmail.com>
2022-02-21 09:28:06 +0100

    Fix certificate deployment script

--- scripts/createTlsSecret.sh
+++ scripts/createTlsSecret.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 mkdir -p ./certs
 if [ x${TLS_CERT_FILE} = x ] ; then
@@ -11,7 +11,7 @@ if [ x${TLS_CERT_FILE} = x ] ; then
   openssl req -subj '/C=US' -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -keyout certs/tls.key -out certs/tls.crt
 fi
 
-oc delete secret kubeframe-ui-certs -n kubeframe-ui
+oc delete secret kubeframe-ui-certs -n kubeframe-ui || true
 oc create secret tls kubeframe-ui-certs -n kubeframe-ui \
   --cert=${TLS_CERT_FILE} \
   --key=${TLS_KEY_FILE}
