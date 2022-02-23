7acf5ce35463226d4a1c5a5bcf81483cc3e39e20
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-02-23 17:11:02 +0100

    fix error crypto engine with skopeo
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- deploy-disconnected-registry/olm-sync.sh
+++ deploy-disconnected-registry/olm-sync.sh
@@ -175,8 +175,8 @@ function mirror() {
                 if [ ${MATCH} == 0 ]; then
                     echo
                     echo "Package: ${package}"
-                    echo "DEBUG: skopeo copy docker://${package} docker://${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS}/$(echo $package | awk -F'/' '{print $2}')-$(basename $package) --all --authfile ${PULL_SECRET}"
-                    skopeo copy docker://${package} docker://${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS}/$(echo $package | awk -F'/' '{print $2}')-$(basename $package) --all --authfile ${PULL_SECRET}
+                    echo "DEBUG: skopeo copy --remove-signatures docker://${package} docker://${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS}/$(echo $package | awk -F'/' '{print $2}')-$(basename $package) --all --authfile ${PULL_SECRET}"
+                    skopeo copy --remove-signatures docker://${package} docker://${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS}/$(echo $package | awk -F'/' '{print $2}')-$(basename $package) --all --authfile ${PULL_SECRET}
                     if [[ ${?} != 0 ]]; then
                         echo "Error on Image Copy, retrying after 5 seconds..."
                         sleep 10
