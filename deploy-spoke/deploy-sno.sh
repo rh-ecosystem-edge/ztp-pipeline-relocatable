45d8d51d51e445e9503483ecfc61e29b96470e7f
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-02-24 17:46:43 +0100

    add verify for new sno feature Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

---
+++ deploy-spoke/deploy-sno.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+set -m
+
+# variables
+# #########
+# uncomment it, change it or get it from gh-env vars (default behaviour: get from gh-env)
+# export KUBECONFIG=/root/admin.kubeconfig
+
+# Load common vars
+source ${WORKDIR}/shared-utils/common.sh
+
+if ! ./verify-sno.sh 1; then
+    echo ">>>> Deploy all the manifests using kustomize"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+    oc patch provisioning provisioning-configuration --type merge -p '{"spec":{"watchAllNamespaces": true}}'
+
+    cd ${OUTPUTDIR}
+    oc apply -k .
+
+    echo "Verifying again the clusterDeployment"
+    # Waiting for 166 min to have the cluster deployed
+    ${WORKDIR}/${DEPLOY_SPOKES_DIR}/verify-sno.sh 10000
+else
+    echo ">> Cluster deployed, this step is not neccessary"
+    exit 0
+fi
