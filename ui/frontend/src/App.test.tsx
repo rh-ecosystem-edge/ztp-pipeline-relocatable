a02f37cc0c1006dc1318547e2fefc77941a9d600
Marek Libra <marek.libra@gmail.com>
2022-03-02 09:13:38 +0100

    UI: Add yarn test to top-level package.json

--- ui/frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('TODO: write App.tsx test', () => {
-  render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
