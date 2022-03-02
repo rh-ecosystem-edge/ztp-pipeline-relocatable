f69e62a7422e5e76fdf24c3b3a740c71df9789b4
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2022-03-02 11:13:03 +0100

    Change way of finding certificates to approve

--- deploy-spoke/csr_autoapprover.sh
+++ deploy-spoke/csr_autoapprover.sh
@@ -1,34 +1,32 @@
 #!/bin/sh
 
 # Wait for API to come online
-until [ $(curl -k -s https://api:6443/version?timeout=10s | jq -r '.major' | grep -v null | wc -l) -eq 1 ]
-do
-  echo "Waiting for API..."
-  sleep 10
+until [ $(curl -k -s https://api:6443/version?timeout=10s | jq -r '.major' | grep -v null | wc -l) -eq 1 ]; do
+    echo "Waiting for API..."
+    sleep 10
 done
 
 # Begin looking for and signing CSRs to activate nodes
 CLUSTER_DATA_FOLDER="/var/home/core/cluster_access_data"
-export KUBECONFIG=$(ls $CLUSTER_DATA_FOLDER/*/kubeconfig-*|head -1)
-
+export KUBECONFIG=$(ls $CLUSTER_DATA_FOLDER/*/kubeconfig-* | head -1)
 
 if [ ! -f "${KUBECONFIG}" ]; then
-  echo "ERROR: Could not find kubeconfig file for cluster"
-  exit 1
+    echo "ERROR: Could not find kubeconfig file for cluster"
+    exit 1
 fi
 
 count=30
-while [ ${count} -gt 0 ]
-do
+while [ ${count} -gt 0 ]; do
 
-  oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | grep -E "kubelet-serving|kube-apiserver-client-kubelet" | xargs oc adm certificate approve
+    # We need to get the Pending CSR's and work over them to approve the individual certificates
+    oc get csr | grep Pending | grep -E 'kube-apiserver-client|kubelet-serving' | awk '{print $1}' | xargs oc adm certificate approve
 
-  if [ $(oc get csr | grep Approved | grep -v Issued | wc -l) -gt 0 ]; then
-    echo "Waiting for certificate requests and issued certificates..."
-  else
-    echo "No pending/unissued certificate requests (CSR) found."
-    count=$((${count}-1))
-  fi
-  sleep 20
+    if [ $(oc get csr | grep Approved | grep -v Issued | wc -l) -gt 0 ]; then
+        echo "Waiting for certificate requests and issued certificates..."
+    else
+        echo "No pending/unissued certificate requests (CSR) found."
+        count=$((count - 1))
+    fi
+    sleep 20
 
 done
