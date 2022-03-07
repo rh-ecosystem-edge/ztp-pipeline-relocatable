5e80851a30130d894fba8b0e62025fb16a10f8d3
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2022-03-07 21:30:31 +0100

    Fixing a new bug from an MCP issue, some pods from OCS are stuck evicting themselves
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- deploy-disconnected-registry/deploy.sh
+++ deploy-disconnected-registry/deploy.sh
@@ -42,32 +42,37 @@ function side_evict_error() {
 
     KUBEC=${1}
     echo ">> Looking for eviction errors"
-    pattern='SchedulingDisabled'
+    status='SchedulingDisabled'
 
-    conflicting_node="$(oc --kubeconfig=${KUBEC} get node --no-headers | grep ${pattern} | cut -f1 -d\ )"
+    conflicting_node="$(oc --kubeconfig=${KUBEC} get node --no-headers | grep ${status} | cut -f1 -d\ )"
 
     if [[ -z ${conflicting_node} ]]; then
-        echo "No masters on ${pattern}"
+        echo "No masters on ${status}"
     else
         conflicting_daemon_pod=$(oc --kubeconfig=${KUBEC} get pod -n openshift-machine-config-operator -o wide --no-headers | grep daemon | grep ${conflicting_node} | cut -f1 -d\ )
-        log_entry="$(oc --kubeconfig=${KUBEC} logs -n openshift-machine-config-operator ${conflicting_daemon_pod} -c machine-config-daemon | grep drain.go | grep evicting | tail -1 | grep pods)"
+        pattern_1="$(oc --kubeconfig=${KUBEC} logs -n openshift-machine-config-operator ${conflicting_daemon_pod} -c machine-config-daemon | grep drain.go | grep evicting | tail -1 | grep pods)"
+        pattern_2="$(oc --kubeconfig=${KUBEC} logs -n openshift-machine-config-operator ${conflicting_daemon_pod} -c machine-config-daemon | grep drain.go | grep "Draining failed" | tail -1 | grep pod)"
 
-        if [[ -z ${log_entry} ]]; then
-            echo "No Conflicting LogEntry on ${conflicting_daemon_pod}"
-        else
-            echo ">> Conflicting LogEntry Found!!"
-            pod=$(echo ${log_entry##*pods/} | cut -d\" -f2)
-            conflicting_ns=$(oc --kubeconfig=${KUBEC} get pod -A | grep ${pod} | cut -f1 -d\ )
+        for log_entry in "${pattern_1}" "${pattern_2}"
+        do
+                if [[ -z ${log_entry} ]]; then
+                    echo "No Conflicting LogEntry on ${conflicting_daemon_pod}"
+                else
+                    echo ">> Conflicting LogEntry Found!!"
+                    pod=$(echo ${log_entry##*pods/} | cut -d\" -f2)
+                    conflicting_ns=$(oc --kubeconfig=${KUBEC} get pod -A | grep ${pod} | cut -f1 -d\ )
 
-            echo ">> Clean Eviction triggered info: "
-            echo NODE: ${conflicting_node}
-            echo DAEMON: ${conflicting_daemon_pod}
-            echo NS: ${conflicting_ns}
-            echo LOG: ${log_entry}
-            echo POD: ${pod}
+                    echo ">> Clean Eviction triggered info: "
+                    echo NODE: ${conflicting_node}
+                    echo DAEMON: ${conflicting_daemon_pod}
+                    echo NS: ${conflicting_ns}
+                    echo LOG: ${log_entry}
+                    echo POD: ${pod}
 
-            oc --kubeconfig=${KUBEC} delete pod -n ${conflicting_ns} ${pod}
-        fi
+                    oc --kubeconfig=${KUBEC} delete pod -n ${conflicting_ns} ${pod} --force --grace-period=0
+                fi
+        done
+
     fi
 }
 
