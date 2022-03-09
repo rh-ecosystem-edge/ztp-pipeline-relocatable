6f789ed0994e144b383ca63475bfab8438e06b24
Marek Libra <marek.libra@gmail.com>
2022-03-09 11:47:24 +0100

    fix typo

--- ui/frontend/src/components/FinalPage/FinalPageSummary.tsx
+++ ui/frontend/src/components/FinalPage/FinalPageSummary.tsx
@@ -31,13 +31,13 @@ export const FinalPageSummary: React.FC = () => {
           Setup complete!
         </Title>
         <Text component={TextVariants.small}>
-          Your Kuberame has been successfully configured and is ready to use.
+          Your cluster has been successfully configured and is ready to use.
         </Text>
       </StackItem>
       <StackItem className="final-page-sumamary__item">
         <Title headingLevel="h3">Enjoy your KubeFrame</Title>
         <Text component={TextVariants.small}>
-          MAC Your Kuberame has been successfully configured and is ready to use.
+          Your cluster has been successfully configured and is ready to use.
         </Text>
       </StackItem>
       <StackItem className="final-page-sumamary__item">
