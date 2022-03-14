e1642af62097a8db262b2a5886c5b1d216123472
alknopfler <alknopfler@users.noreply.github.com>
2022-03-14 20:40:02 +0100

    fix error with dns naming in UI
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- hack/deploy-hub-local/build-spoke.sh
+++ hack/deploy-hub-local/build-spoke.sh
@@ -140,6 +140,7 @@ kcli create dns -n bare-net api.spoke0-cluster.alklabs.com -i 192.168.150.201
 kcli create dns -n bare-net api-int.spoke0-cluster.alklabs.com -i 192.168.150.201
 kcli create dns -n bare-net ztpfw-registry-ztpfw-registry.apps.spoke0-cluster.alklabs.com -i 192.168.150.200
 kcli create dns -n bare-net ztpfw-ui-ztpfw-ui.apps.spoke0-cluster.alklabs.com -i 192.168.150.200
+kcli create dns -n bare-net edge-cluster-setup.apps.spoke0-cluster.alklabs.com -i 192.168.150.200
 kcli create dns -n bare-net ztpfw-registry-quay-ztpfw-registry.apps.spoke0-cluster.alklabs.com -i 192.168.150.200
 kcli create dns -n bare-net noobaa-mgmt-openshift-storage.apps.spoke0-cluster.alklabs.com -i 192.168.150.200
 kcli create dns -n bare-net console-openshift-console.apps.spoke0-cluster.alklabs.com -i 192.168.150.200
