0f197c33e3611b7307e566e0131cf3f8625e2362
Marek Libra <marek.libra@gmail.com>
2022-03-24 13:08:32 +0100

    MGMT-9698 Revoke kubeadmin password
    The kubeadmin password is revoked once new user with cluster-admin role
    is created.

--- ui/frontend/src/components/PersistPage/constants.ts
+++ ui/frontend/src/components/PersistPage/constants.ts
@@ -3,6 +3,7 @@ export const RESOURCE_PATCH_TITLE = 'Resource update failed';
 export const MISSING_VALUE = 'Missing value';
 export const RESOURCE_FETCH_TITLE = 'Failed to read resource';
 export const PERSIST_IDP = 'Registering new identity HTPasswd provider failed.';
+export const KUBEADMIN_REMOVE = 'Kubeadmin removal failed.';
 
 export const IDENTITY_PROVIDER_NAME = 'ztpfw-htpasswd-idp';
 
