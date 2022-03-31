e7cc0d28f8898a7808d54f281afd01590fd4fad0
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-03-31 10:48:35 +0200

    fix error with resources in tekton
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- deploy-disconnected-registry/olm-sync.sh
+++ deploy-disconnected-registry/olm-sync.sh
@@ -157,8 +157,8 @@ function mirror() {
 
     # Mirror redhat-operator packages
     echo ">>>> Trying to push OLM images to Internal Registry"
-    echo "DEBUG: GODEBUG=x509ignoreCN=0 oc adm catalog mirror ${OLM_DESTINATION_INDEX} ${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS} --registry-config=${PULL_SECRET}"
-    GODEBUG=x509ignoreCN=0 oc --kubeconfig=${TARGET_KUBECONFIG} adm catalog mirror ${OLM_DESTINATION_INDEX} ${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS} --registry-config=${PULL_SECRET} --max-per-registry=100 >>${OUTPUTDIR}/mirror.log 2>&1
+    echo "oc adm catalog mirror ${OLM_DESTINATION_INDEX} ${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS} --registry-config=${PULL_SECRET} --max-per-registry=100"
+    oc --kubeconfig=${TARGET_KUBECONFIG} adm catalog mirror ${OLM_DESTINATION_INDEX} ${DESTINATION_REGISTRY}/${OLM_DESTINATION_REGISTRY_IMAGE_NS} --registry-config=${PULL_SECRET} --max-per-registry=100 >>${OUTPUTDIR}/mirror.log 2>&1
 
     cat ${OUTPUTDIR}/mirror.log | grep 'error:' >${OUTPUTDIR}/mirror-error.log
 
