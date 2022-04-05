2575d2eb2b41af06df02a121ede3f52a9daa7e0f
Daniel Chavero <dchavero@redhat.com>
2022-04-05 11:47:15 +0200

    (WIP) add extra gpu operator folder
    Co-authored-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

---
+++ extras/gpu-operator/verify.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+set -m
+
+# variables
+# #########
+# uncomment it, change it or get it from gh-env vars (default behaviour: get from gh-env)
+# export KUBECONFIG=/root/admin.kubeconfig
+
+# Load common vars
+source ${WORKDIR}/shared-utils/common.sh
+exit 0
+
