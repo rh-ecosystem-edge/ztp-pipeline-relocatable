398ce0e1c5b152955893732f4ec080b6598040a9
Daniel Chavero <ydchavero@redhat.com>
2022-04-08 10:56:49 +0000

    feat: deploy gpu operator on all spokes

--- contrib/gpu-operator/deploy.sh
+++ contrib/gpu-operator/deploy.sh
@@ -4,12 +4,14 @@ set -o pipefail
 set -o nounset
 set -m
 
+source ${WORKDIR}/shared-utils/common.sh
+
 function wait_for_crd() {
     SPOKE_KUBECONFIG=${1}
-    SPOKE_NAME=${2}
+    SPOKE=${2}
     CRD=${3}
 
-    echo ">>>> Waiting for subscription and crd on: ${SPOKE_NAME}"
+    echo ">>>> Waiting for subscription and crd on: ${SPOKE}"
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     timeout=0
     ready=false
@@ -30,51 +32,67 @@ function wait_for_crd() {
     fi
 }
 
-# Parse args
-if [ $# -eq 0 ]; then
-  echo "No arguments supplied. Usage $0 <Kubeconfig file path> "
-  echo "  e.g.: ./deploy.sh /home/user/spoke1-kubeconfig"
-  exit 1
-fi
-
-export SPOKE_KUBECONFIG=$1
-export SPOKE=$(oc config get-clusters | sed 1d)
-
-# Load common vars
-# source ${WORKDIR}/shared-utils/common.sh
-
-if ./verify.sh; then
-
-    echo "Installing NFD operator for ${SPOKE}"
-    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/01-nfd-namespace.yaml
-    sleep 2
-    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/02-nfd-operator-group.yaml
-    sleep 2
-    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/03-nfd-subscription.yaml
-    sleep 2
+function get_config() {
     
-    wait_for_crd ${SPOKE_KUBECONFIG} ${SPOKE} "nodefeaturediscoveries.nfd.openshift.io"
+    if [[ $# -lt 1 ]]; then
+        echo "Usage :"
+        echo "  $0 <SPOKES_FILE> <SPOKE_NAME> <SPOKE_INDEX>"
+        exit 1
+    fi
+    SPOKE_FILE=${1}
+    SPOKE_NAME=${2}
+    SPOKE_INDEX=${3}
+    
+    export CHANGEME_VERSION=$(yq eval ".spokes[${index}].${spoke_name}.contrib.gpu-operator.version" ${SPOKES_FILE})
+}
 
-    echo "Installing GPU operator for ${SPOKE}"
-    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/01-gpu-namespace.yaml
-    sleep 2
-    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/02-gpu-operator-group.yaml
-    sleep 2
-    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/03-gpu-subscription.yaml
-    sleep 2
+function deploy_gpu() {
+    if ./verify.sh; then
 
-    wait_for_crd ${SPOKE_KUBECONFIG} ${SPOKE} "clusterpolicies.nvidia.com"
+        echo "Installing NFD operator for ${spoke}"
+        oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/01-nfd-namespace.yaml
+        sleep 2
+        oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/02-nfd-operator-group.yaml
+        sleep 2
+        oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/03-nfd-subscription.yaml
+        sleep 2
 
-    echo "Adding GPU node labels with NFD for ${SPOKE}"
-    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/04-nfd-gpu-feature.yaml
-    sleep 2
+        wait_for_crd ${SPOKE_KUBECONFIG} ${spoke} "nodefeaturediscoveries.nfd.openshift.io"
 
-    echo "Adding GPU Cluster Policy for ${SPOKE}"
-    oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/05-gpu-cluster-policy.yaml
-    sleep 2
-else
-    echo ">>>> This step is not neccesary, everything looks ready"
+        echo "Installing GPU operator for ${spoke}"
+        oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/01-gpu-namespace.yaml
+        sleep 2
+        oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/02-gpu-operator-group.yaml
+        sleep 2
+        oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/03-gpu-subscription.yaml
+        sleep 2
+
+        wait_for_crd ${SPOKE_KUBECONFIG} ${spoke} "clusterpolicies.nvidia.com"
+
+        echo "Adding GPU node labels with NFD for ${spoke}"
+        oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f manifests/04-nfd-gpu-feature.yaml
+        sleep 2
+
+        echo "Adding GPU Cluster Policy for ${spoke}"
+        envsubst <manifests/05-gpu-cluster-policy.yaml | oc --kubeconfig=${SPOKE_KUBECONFIG} apply -f -
+        sleep 2
+    else
+        echo ">>>> This step is not neccesary, everything looks ready"
+    fi
+
+    echo ">>>>EOF"
+    echo ">>>>>>>"
+}
+
+if [[ -z ${ALLSPOKES} ]]; then
+    ALLSPOKES=$(yq e '(.spokes[] | keys)[]' ${SPOKES_FILE})
 fi
 
-echo ">>>>EOF"
-echo ">>>>>>>"
+index=0
+for spoke in ${ALLSPOKES}; do
+    extract_kubeconfig_common ${spoke}
+    get_config ${SPOKES_FILE} ${spoke} ${index}
+    deploy_gpu ${spoke}
+    index=$((index + 1))
+    echo ">> GPU Operator Deployment done in: ${spoke}"
+done
