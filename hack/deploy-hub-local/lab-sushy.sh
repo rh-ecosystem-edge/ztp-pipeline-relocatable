8c6c315d536815bcf69a14a00571382c83a61173
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-04-12 09:35:06 +0200

    fix sushy installing directly in host as requested
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- hack/deploy-hub-local/lab-sushy.sh
+++ hack/deploy-hub-local/lab-sushy.sh
@@ -3,18 +3,16 @@
 PS_SUSHY=$(ps -ef | grep py | grep sushy)
 if [ -z "$PS_SUSHY" ]; then
   cp ./lab-sushy.conf /etc/sushy.conf
+  cp ./lab-sushy.service /etc/systemd/system/sushy-emulator.service
 
-  dnf -y install podman pkgconf-pkg-config libvirt-devel gcc python3-libvirt python3 git python3-netifaces
+  dnf -y install pkgconf-pkg-config libvirt-devel gcc python3-libvirt python3 git python3-netifaces
 
-  export SUSHY_TOOLS_IMAGE=${SUSHY_TOOLS_IMAGE:-"quay.io/metal3-io/sushy-tools"}
-  podman create --net host --privileged --name sushy-emulator -v "/etc":/etc -v "/var/run/libvirt":/var/run/libvirt "${SUSHY_TOOLS_IMAGE}" sushy-emulator -i :: -p 8000 --config /etc/sushy.conf
-
-  podman generate systemd --restart-policy=always -t 1 sushy-emulator > /etc/systemd/system/sushy-emulator.service
   systemctl daemon-reload
   systemctl enable --now sushy-emulator.service
-  sleep 10
-  systemctl start sushy-emulator.service
 
   firewall-cmd --zone=libvirt --permanent --add-port=8000/tcp
-
+  firewall-cmd --reload
+  systemctl start sushy-emulator.service
+else
+  echo "Sushy is already running"
 fi
