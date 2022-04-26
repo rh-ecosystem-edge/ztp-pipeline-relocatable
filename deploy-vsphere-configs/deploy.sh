aa940183146bede06a79fbce0a00d704c5903827
Tosin Akinosho <takinosh@redhat.com>
2022-04-26 09:27:50 -0400

    testing vsphere configs

---
+++ deploy-vsphere-configs/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+set -o errexit
+set -m
+
+# variables
+# #########
+# uncomment it, change it or get it from gh-env vars (default behaviour: get from gh-env)
+# export KUBECONFIG=/root/admin.kubeconfig
+
+if ./verify.sh; then
+
+    # Load common vars
+    source ${WORKDIR}/shared-utils/common.sh
+
+    git clone https://github.com/Red-Hat-SE-RTO/openshift-ztp.git
+    cd openshift-ztp
+    pip3 install -r ./requirements.txt
+elif [[ $? -eq 50 ]]; then
+    echo ">>>> Verify failed...Some pods are failing..." #TODO change to remove and launch again
+    exit 50
+else
+    echo ">>>> This step is not neccesary, everything looks ready"
+fi
+echo ">>>>EOF"
+echo ">>>>>>>"
