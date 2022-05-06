ef89075998be823841dcafdfe99d29accff1ed76
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-05-06 16:47:48 +0200

    color format
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- shared-utils/wait_for_sno_mco.sh
+++ shared-utils/wait_for_sno_mco.sh
@@ -7,37 +7,33 @@ set -m
 tput setab 7
 tput setaf 4
 
-echo ">>>> Verify the api active until mco restart node"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 retry=1
   while [ ${retry} != 0 ]; do
     oc --kubeconfig=${KUBECONFIG} get nodes &> /dev/null
     if [[ $? == 0 ]]; then
-       echo "Api is up...waiting until mco will restart the node"
+       echo "$(tput setab 7)$(tput setaf 4)[ZTPFW - Monitoring MCO - SNO]$(tput sgr 0) >>>> API is up...waiting until mco will restart the node"
        sleep 10
        retry=$((retry + 1))
     else
-       echo ">>>> API is down...The Cluster is restarting"
+       echo "$(tput setab 7)$(tput setaf 4)[ZTPFW - Monitoring MCO - SNO]$(tput sgr 0) >>>> API is down...The Cluster is restarting"
        retry=0
     fi
   done
 
-echo ">>>> Wait until sno is restarted"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 retry=1
   while [ ${retry} != 0 ]; do
     oc --kubeconfig=${KUBECONFIG} get nodes &> /dev/null
     if [[ $? != 0 ]]; then
-        echo "Api is down...waiting until node will be restarted"
+        echo "$(tput setab 7)$(tput setaf 4)[ZTPFW - Monitoring MCO - SNO]$(tput sgr 0) >>>> Api is down...waiting until node will be restarted"
         sleep 10
         retry=$((retry + 1))
     else
-        echo ">>>> API is up again...The Cluster is restarted"
+        echo "$(tput setab 7)$(tput setaf 4)[ZTPFW - Monitoring MCO - SNO]$(tput sgr 0) >>>> API is up again...The Cluster is restarted"
         retry=0
     fi
   done
 
-echo ">>>> Launch again the pipeline"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+sleep 120
+echo "$(tput setab 7)$(tput setaf 4)[ZTPFW - Monitoring MCO - SNO]$(tput sgr 0) >>>> Relaunch again the pipeline"
 make deploy-pipe-hub-sno
 
