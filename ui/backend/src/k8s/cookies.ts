f3316bd0691b1bbc825c05c3180fb0328af7ea14
Marek Libra <marek.libra@gmail.com>
2022-05-06 10:40:44 +0200

    MGMT-9937: Fix Logout (#200)
    * MGMT-9937: Fix Logout
    
    * Update snapshots
    
    * Add forgotten "configure" endpoint
    
    Co-authored-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- ui/backend/src/k8s/cookies.ts
+++ ui/backend/src/k8s/cookies.ts
@@ -16,12 +16,35 @@ export function parseCookies(req: Request): Record<string, string> {
 }
 
 export function setCookie(res: Response, cookie: string, value: string, path?: string): void {
-  res.setHeader('Set-Cookie', `${cookie}=${value}; Secure; HttpOnly; Path=${path ? path : '/'}`);
+  const cookieString = `${cookie}=${value}; Secure; HttpOnly; Path=${path ? path : '/'}`;
+  const cookieHeader = res.getHeader('Set-Cookie');
+  if (cookieHeader) {
+    if (Array.isArray(cookieHeader)) {
+      res.setHeader('Set-Cookie', [...cookieHeader, cookieString]);
+    } else {
+      res.setHeader('Set-Cookie', [cookieHeader as string, cookieString]);
+    }
+  } else {
+    res.setHeader('Set-Cookie', cookieString);
+  }
 }
 
-export function deleteCookie(res: Response, cookie: string, path?: string): void {
-  res.setHeader(
-    'Set-Cookie',
-    `${cookie}=; Secure; HttpOnly; Path=${path ? path : '/'}` + `; max-age=0`,
-  );
+export function deleteCookie(
+  res: Response,
+  options: { cookie: string; path?: string; domain?: string },
+): void {
+  let cookieString =
+    `${options.cookie}=; Secure; HttpOnly; Path=${options.path ? options.path : '/'}` +
+    `; max-age=0`;
+  if (options.domain) cookieString += `; Domain=${options.domain}`;
+  const cookieHeader = res.getHeader('Set-Cookie');
+  if (cookieHeader) {
+    if (Array.isArray(cookieHeader)) {
+      res.setHeader('Set-Cookie', [...cookieHeader, cookieString]);
+    } else {
+      res.setHeader('Set-Cookie', [cookieHeader as string, cookieString]);
+    }
+  } else {
+    res.setHeader('Set-Cookie', cookieString);
+  }
 }
