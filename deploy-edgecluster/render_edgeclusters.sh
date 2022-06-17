131a57353a0017d136a482688a5924d621bd7ac0
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-06-17 09:57:25 +0200

    feature: add MCE
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- deploy-edgecluster/render_edgeclusters.sh
+++ deploy-edgecluster/render_edgeclusters.sh
@@ -143,6 +143,7 @@ EOF
   apiVIP: "$CHANGE_EDGE_API"
   ingressVIP: "$CHANGE_EDGE_INGRESS"
   networking:
+    networkType: OpenShiftSDN
     clusterNetwork:
       - cidr: "$CHANGE_EDGE_CLUSTER_NET_CIDR"
         hostPrefix: $CHANGE_EDGE_CLUSTER_NET_PREFIX
