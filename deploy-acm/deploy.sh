ae5958eeaf04f6425b3452c7edc0f4c9658891e8
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-06-20 12:54:19 +0200

    feature: add MCE
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- deploy-acm/deploy.sh
+++ deploy-acm/deploy.sh
@@ -40,11 +40,11 @@ if ./verify.sh; then
 
     echo ">>>> Wait until RHACM ready"
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    for helmchart in $(oc --kubeconfig=${KUBECONFIG_HUB} get helmreleases -o name); do
-        RESOURCE_KIND=${helmchart%%/*}
-        RESOURCE_NAME=${helmchart##*/}
-        check_resource "${RESOURCE_KIND}" "${RESOURCE_NAME}" "Deployed" "open-cluster-management" "${KUBECONFIG_HUB}"
-    done
+    for dep in $(oc --kubeconfig=${KUBECONFIG_HUB} get deployment -n open-cluster-management -o name); do
+            RESOURCE_KIND=${dep%%/*}
+            RESOURCE_NAME=${dep##*/}
+            check_resource "${RESOURCE_KIND}" "${RESOURCE_NAME}" "Available" "open-cluster-management" "${KUBECONFIG_HUB}"
+        done
 else
     echo ">>>> This step is not neccesary, everything looks ready"
 fi
