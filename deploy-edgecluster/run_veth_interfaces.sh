b48fae8f0b291bbd6f8c466e26a4ca0d7eb802b2
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-07-12 11:39:05 +0200

    add ign override systemd veth
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- deploy-edgecluster/run_veth_interfaces.sh
+++ deploy-edgecluster/run_veth_interfaces.sh
@@ -4,4 +4,4 @@ export PATH=/root/.local/bin:/root/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin
 
 ip link set dev veth2 1>/dev/null 2>&1
 sleep 5
-ip link set dev veth1>/dev/null 2>&1
+ip link set dev veth1 1>/dev/null 2>&1
