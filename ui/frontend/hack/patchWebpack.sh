f2b498aa35f53dbe70cd5c8292249b38c9fad34c
Marek Libra <marek.libra@gmail.com>
2022-07-12 16:23:46 +0200

    Patch frontend's webpack.config.js
    To resolve missing sourcemap error for ip-address package

---
+++ ui/frontend/hack/patchWebpack.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# exclude: /@babel(?:\/|\\{1,2})runtime/,
+# exclude: /@babel(?:\/|\\{1,2})runtime|ip-address/
+
+COUNT=$(grep 'exclude: /@babel(?:\\/|\\\\{1,2})runtime/,' node_modules/react-scripts/config/webpack.config.js | wc -l)
+
+set -x
+
+if [ x${COUNT} = x2 ] ; then
+  sed  -i "s|)runtime/,$|)runtime\|ip-address/,|g" node_modules/react-scripts/config/webpack.config.js
+else
+  echo '== Unable to patch webpack.config.js'
+fi
+
+
