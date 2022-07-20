74a7c22ef8fc7cbf10d339f815fc73f31b314d59
Tosin Akinosho <takinosh@redhat.com>
2022-07-20 14:09:09 -0400

    adding aap argocd and gitea for vsphere

---
+++ contrib/deploy-aap/verify.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+set -m
+
+# variables
+# #########
+# uncomment it, change it or get it from gh-env vars (default behaviour: get from gh-env)
+# export KUBECONFIG=/root/admin.kubeconfig
+
+# Load common vars
+source ${WORKDIR}/shared-utils/common.sh
+
+echo "development"
+echo ">>>>EOF"
+echo ">>>>>>>"
