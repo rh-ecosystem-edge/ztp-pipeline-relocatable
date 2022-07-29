6d57872dd2e4925b6945e03c0c62ef6cf22c69ae
Tosin Akinosho <takinosh@redhat.com>
2022-07-29 13:23:10 -0400

    adding mock variable

--- hack/deploy-hub-local/vsphere-install.sh
+++ hack/deploy-hub-local/vsphere-install.sh
@@ -3,7 +3,7 @@
 ## 1. Deploy cluster using https://github.com/kenmoini/ocp4-ai-svc-libvirt
 ## 2. Configure ODF
 ## 3. git clone Git clone https://github.com/tosin2013/ztp-pipeline-relocatable.git && git checkout vsphere
-## 4.create vshpere-clusters.yaml
+## 4. create vshpere-clusters.yaml
 ## 5. Start Script below 
 ####
 tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults common-pre-flight
