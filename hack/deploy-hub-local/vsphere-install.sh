1aada8a4e0b98dac3b754de87bb4f2812d1f0295
Tosin Akinosho <takinosh@redhat.com>
2022-07-29 15:24:43 -0400

    updating deploy.sh script

--- hack/deploy-hub-local/vsphere-install.sh
+++ hack/deploy-hub-local/vsphere-install.sh
@@ -5,11 +5,21 @@
 ## 3. git clone Git clone https://github.com/tosin2013/ztp-pipeline-relocatable.git && git checkout vsphere
 ## 4. create vshpere-clusters.yaml
 ## 5. Start Script below 
+## Testing connected install first
 ####
-tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults common-pre-flight
-tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults hub-deploy-httpd-server
-tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults hub-deploy-disconnected-registry
-tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults hub-deploy-acm
-tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults deploy-openshift-ztp-aap
+export DISCONNECT_INSTALL=false
+
+if [ "$DISCONNECT_INSTALL" = true ]; then
+    echo "Desconnected pipelines for  vSphere"
+    tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults common-pre-flight
+    tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults hub-deploy-httpd-server
+    tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults hub-deploy-disconnected-registry
+    tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults hub-deploy-acm
+    tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults deploy-openshift-ztp-aap
+else
+    tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults common-pre-flight
+    tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults hub-deploy-acm
+    tkn task start -n edgecluster-deployer -p ztp-container-image="quay.io/takinosh/pipeline:vsphere" -p edgeclusters-config="$(cat  vshpere-clusters.yaml)" -p kubeconfig=${KUBECONFIG} -w name=ztp,claimName=ztp-pvc --timeout 5h --use-param-defaults deploy-openshift-ztp-aap
+fi
 
 
