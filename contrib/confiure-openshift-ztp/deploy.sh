9d734f2e5504e1a196e6b39a52f542cbcb2d52f8
Tosin Akinosho <takinosh@redhat.com>
2022-08-08 12:05:30 -0400

    adding configure openshift for vsphere

---
+++ contrib/confiure-openshift-ztp/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -m
+
+
+
+if ./verify.sh; then
+    # Load common vars
+    source ${WORKDIR}/shared-utils/common.sh
+    echo ">>>> Deploy manifests to create template namespace on HUB Cluster"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+    ##############################################################################
+    # Here can be added other manifests to create the required resources
+    ##############################################################################
+
+    pip install ansible 
+    ansible -v
+
+    ##############################################################################
+    # End of customization
+    ##############################################################################
+
+fi
+
+echo ">>>>EOF"
+echo ">>>>>>>"
