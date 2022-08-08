6e4572fc16ff18ce799981ed1b3ae165ef69933a
Tosin Akinosho <takinosh@redhat.com>
2022-08-08 12:42:13 -0400

    updating code for reflector

--- contrib/configure-openshift-ztp/deploy.sh
+++ contrib/configure-openshift-ztp/deploy.sh
@@ -5,26 +5,20 @@ set -o pipefail
 set -o nounset
 set -m
 
+
+
 if ./verify.sh; then
     # Load common vars
     source ${WORKDIR}/shared-utils/common.sh
-
-    echo ">>>> Deploy manifests to create template namespace in: HUB Cluster"
+    echo ">>>> Deploy manifests to create template namespace on HUB Cluster"
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    echo "Extract Kubeconfig for HUB Cluster"
-    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
     ##############################################################################
     # Here can be added other manifests to create the required resources
     ##############################################################################
 
-    oc --kubeconfig=${KUBECONFIG_HUB} apply -f manifests/01-namespace.yml
-    helm list 
-    sleep 2
-    helm repo add emberstack https://emberstack.github.io/helm-charts  --namespace reflector
-    sleep 2
-    helm repo update --namespace reflector
-    sleep 2
-    helm upgrade --install reflector emberstack/reflector --namespace reflector
+    pip install ansible 
+    ansible -v
 
     ##############################################################################
     # End of customization
