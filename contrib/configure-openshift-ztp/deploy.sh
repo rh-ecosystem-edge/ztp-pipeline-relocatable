32763264a69e6cefa2ffbc591bb34ccc5a98cfe2
Tosin Akinosho <takinosh@redhat.com>
2022-08-11 11:33:57 -0400

    adding s3 download for tower manifest

--- contrib/configure-openshift-ztp/deploy.sh
+++ contrib/configure-openshift-ztp/deploy.sh
@@ -33,12 +33,22 @@ if ./verify.sh; then
     ## Install needed Ansible Collections
     ansible-galaxy collection install -r ./collections/requirements.yml
 
+    ## Collecting Manifest.zip for AAP
+    YOUR_OSC_ACCESS_KEY=$( oc -n edgecluster-deployer get secret openshift-zip-configs  -o jsonpath="{.data.AWS_ACCESS_KEY_ID}" | base64 --decode)
+    YOUR_OSC_SECRET_KEY=$( oc -n edgecluster-deployer get secret openshift-zip-configs  -o jsonpath="{.data.AWS_SECRET_ACCESS_KEY}" | base64 --decode)
+    curl -OL https://raw.githubusercontent.com/tosin2013/openshift-4-deployment-notes/master/aws/configure-aws-cli.sh
+    chmod +x configure-aws-cli.sh 
+    ./configure-aws-cli.sh -i ${YOUR_OSC_ACCESS_KEY} ${YOUR_OSC_SECRET_KEY} us-east-1
+    aws --endpoint-url  https://s3-openshift-storage.apps.rto.tosins-cloudlabs.com/ s3 ls
+    aws  --endpoint-url  https://s3-openshift-storage.apps.rto.tosins-cloudlabs.com/ s3 cp  s3://openshift-zip-configs/manifest_tower-dev_20220811T151908Z.zip .
+
     ## Configure the Hub cluster Operators and Workloads, namely RHACM, AAP2, and RH GitOps (ArgoCD)
     ansible-playbook ansible/2_configure.yaml \
     -e configure_rhacm=true \
     -e configure_aap2_controller=true \
     -e configure_rh_gitops=true \
-    -e pull_secret_path="${PULL_SECRET}" -e 'ansible_python_interpreter=/usr/bin/python3'  -vv
+    -e pull_secret_path="${PULL_SECRET}" -e 'ansible_python_interpreter=/usr/bin/python3'  
+    -e subscription_manifest_path=manifest_tower-dev_20220811T151908Z.zip -vv
 
 
     ##############################################################################
