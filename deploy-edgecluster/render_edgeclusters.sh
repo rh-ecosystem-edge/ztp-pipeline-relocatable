5044cef625cb21d6d3cab125a65614d7f4a57b8e
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-09-13 09:21:32 +0200

    moving tpm config in aci to spec
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- deploy-edgecluster/render_edgeclusters.sh
+++ deploy-edgecluster/render_edgeclusters.sh
@@ -128,8 +128,11 @@ metadata:
   name: $CHANGE_EDGE_NAME
   namespace: $CHANGE_EDGE_NAME
   annotations:
-      agent-install.openshift.io/install-config-overrides: '{"networking":{"networkType":"OVNKubernetes"},"disk_encryption": {"enable_on": "all", "mode": "tpmv2"}}'
+      agent-install.openshift.io/install-config-overrides: '{"networking":{"networkType":"OVNKubernetes"}}'
 spec:
+  diskEncryption:
+      mode: tpmv2
+      enableOn: all
   clusterDeploymentRef:
     name: $CHANGE_EDGE_NAME
 EOF
