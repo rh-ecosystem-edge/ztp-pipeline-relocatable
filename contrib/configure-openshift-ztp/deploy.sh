f8078069f504819e702d8a34832386aeb1a1fd4f
Tosin Akinosho <takinosh@redhat.com>
2022-09-30 11:53:55 -0400

    updating ansbile pipeline

--- contrib/configure-openshift-ztp/deploy.sh
+++ contrib/configure-openshift-ztp/deploy.sh
@@ -33,27 +33,23 @@ if ./verify.sh; then
     ## Install needed Ansible Collections
     ansible-galaxy collection install -r ./collections/requirements.yml
 
-    ## Collecting Manifest.zip for AAP
-    YOUR_OSC_ACCESS_KEY=$( oc -n edgecluster-deployer get secret openshift-zip-configs  -o jsonpath="{.data.AWS_ACCESS_KEY_ID}" | base64 --decode)
-    YOUR_OSC_SECRET_KEY=$( oc -n edgecluster-deployer get secret openshift-zip-configs  -o jsonpath="{.data.AWS_SECRET_ACCESS_KEY}" | base64 --decode)
-    curl -OL https://raw.githubusercontent.com/tosin2013/openshift-4-deployment-notes/master/aws/configure-aws-cli.sh
-    chmod +x configure-aws-cli.sh 
-    export SKIP_CHECK_CALLER_IDENTITY="true"
-    ./configure-aws-cli.sh -i ${YOUR_OSC_ACCESS_KEY} ${YOUR_OSC_SECRET_KEY} us-east-1
-    aws --endpoint-url  https://s3-openshift-storage.apps.rto.tosins-cloudlabs.com/ s3 ls
-    aws  --endpoint-url  https://s3-openshift-storage.apps.rto.tosins-cloudlabs.com/ s3 cp  s3://openshift-zip-configs/manifest_tower-dev_20220811T151908Z.zip ${OUTPUTDIR}/manifest_tower-dev_20220811T151908Z.zip
-
+    ## Collecting Manifest.zip for AA    
     ## Configure the Hub cluster Operators and Workloads, namely RHACM, AAP2, and RH GitOps (ArgoCD)
-    ls -lath ${OUTPUTDIR}
+    ls -lath /opt/ztp
     sleep 5s
 
+    TOWER_MAINFEST_ZIP=$(ls /opt/ztp| grep manifest_tower)
+    if [ -z "${TOWER_MAINFEST_ZIP}" ]; then
+        echo "ERROR: Tower manifest zip file not found in $/opt/ztp"
+        exit 1
+    fi
     ansible-playbook ansible/2_configure.yaml \
     -e configure_rhacm=true \
     -e configure_aap2_controller=true \
     -e configure_rh_gitops=true \
     -e use_services_not_routes=false \
     -e pull_secret_path="${PULL_SECRET}" -e 'ansible_python_interpreter=/usr/bin/python3'  \
-    -e subscription_manifest_path=${OUTPUTDIR}/manifest_tower-dev_20220811T151908Z.zip -vv
+    -e subscription_manifest_path="/opt/ztp/${TOWER_MAINFEST_ZIP}" -vv
 
 
     ##############################################################################
