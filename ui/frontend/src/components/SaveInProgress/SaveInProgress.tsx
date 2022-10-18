976c0c1646007f106bad0cbcc690dc495e95b03c
Marek Libra <marek.libra@gmail.com>
2022-10-18 13:02:06 +0200

    Save In Progress page

--- ui/frontend/src/components/SaveInProgress/SaveInProgress.tsx
+++ ui/frontend/src/components/SaveInProgress/SaveInProgress.tsx
@@ -1,3 +1,26 @@
 import React from 'react';
+import { Bullseye, Flex, FlexItem, Spinner, Text, TextContent } from '@patternfly/react-core';
 
-export const SaveInProgress: React.FC = () => <div>TODO: Save in progress</div>;
+// TODO: Change the picture
+import SittingGuyPict from '../BasicLayout/RedHatLogo.svg';
+
+import './SaveInProgress.css';
+
+export const SaveInProgress: React.FC = () => (
+  <Bullseye>
+    <Flex direction={{ default: 'column' }}>
+      <FlexItem className="save-in-progress-item">
+        <img src={SittingGuyPict} alt="Logo" />
+      </FlexItem>
+      <FlexItem className="save-in-progress-item">
+        <h1>Saving configuration...</h1>
+      </FlexItem>
+      <FlexItem className="save-in-progress-item text-sublabel">
+        This process may take a while. Do not unplug your computer from your edge device.
+      </FlexItem>
+      <FlexItem className="save-in-progress-item">
+        <Spinner isSVG aria-label="Spinner - in progress" />
+      </FlexItem>
+    </Flex>
+  </Bullseye>
+);
