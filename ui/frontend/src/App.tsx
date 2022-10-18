4f5201f1c893b2b355fdbc9cf579bf7afb44bdda
Marek Libra <marek.libra@gmail.com>
2022-10-18 13:29:30 +0200

    Stub all pages

--- ui/frontend/src/App.tsx
+++ ui/frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-// import { WelcomePage } from './components';
-// import { Wizard } from './components/Wizard';
-// import { Settings } from './components/Settings';
-// import { K8SStateContextProvider } from './components/K8SStateContext';
-
 import Redirect from './components/Redirect';
+import {
+  URI_API,
+  URI_CONSOLE,
+  URI_CREDENTIALS,
+  URI_DOMAIN,
+  URI_INGRESS,
+  URI_LAYER3,
+  URI_SSHKEY,
+} from './components/Navigation/routes';
+import { SaveInProgress } from './components/SaveInProgress';
 import { Layer3Page } from './components/Layer3Page/Layer3Page';
 import { IngressPage } from './components/IngressPage';
 
@@ -22,9 +27,17 @@ function App() {
             element={<Redirect to={process.env.REACT_APP_BACKEND_PATH} preservePathName />}
           />
         )}
-        <Route path="/layer3" element={<Layer3Page />} />
-        <Route path="/ingress" element={<IngressPage />} />
-        <Route path="*" element={<Redirect to="/layer3" />} />
+
+        {/* TODO: Replace SaveInProgress by actual implementation */}
+        <Route path={URI_LAYER3} element={<Layer3Page />} />
+        <Route path={URI_INGRESS} element={<IngressPage />} />
+        <Route path={URI_API} element={<SaveInProgress />} />
+        <Route path={URI_DOMAIN} element={<SaveInProgress />} />
+        <Route path={URI_CREDENTIALS} element={<SaveInProgress />} />
+        <Route path={URI_SSHKEY} element={<SaveInProgress />} />
+        <Route path={URI_CONSOLE} element={<SaveInProgress />} />
+
+        <Route path="*" element={<Redirect to={URI_LAYER3} />} />
       </Routes>
     </BrowserRouter>
   );
--- ui/frontend/src/components/Navigation/Navigation.tsx
+++ ui/frontend/src/components/Navigation/Navigation.tsx
@@ -2,25 +2,86 @@ import React from 'react';
 import { Nav, NavGroup, NavItem } from '@patternfly/react-core';
 import { useLocation } from 'react-router-dom';
 
+import {
+  URI_API,
+  URI_CONSOLE,
+  URI_CREDENTIALS,
+  URI_DOMAIN,
+  URI_INGRESS,
+  URI_LAYER3,
+  URI_SSHKEY,
+} from './routes';
+
 import './Navigation.css';
 
 export const Navigation: React.FC = () => {
   const location = useLocation();
   const activeItem = location.pathname;
 
-  const onSelect = (result: { itemId: number | string }) => {
+  const onSelect = (result: { itemId: number | string; to: string }) => {
     // window.location.pathname = result.itemId as string; // Use this if we ever need to keep URL params
-    window.location.href = result.itemId as string; // forget params
+    window.location.href = result.to; // forget params
   };
 
   return (
     <Nav aria-label="Main navigation" theme="light" onSelect={onSelect} className="main-navigation">
       <NavGroup title="Networking">
-        <NavItem preventDefault to="/layer3" itemId="layer3" isActive={activeItem === '/layer3'}>
+        <NavItem
+          preventDefault
+          to={URI_LAYER3}
+          itemId="layer3"
+          isActive={activeItem === URI_LAYER3}
+        >
           TCP/IP
         </NavItem>
-        <NavItem preventDefault to="/ingress" itemId="ingress" isActive={activeItem === '/ingress'}>
+        <NavItem
+          preventDefault
+          to={URI_INGRESS}
+          itemId="ingress"
+          isActive={activeItem === URI_INGRESS}
+        >
           Ingress
+        </NavItem>
+        <NavItem preventDefault to={URI_API} itemId="apiip" isActive={activeItem === URI_API}>
+          API
+        </NavItem>
+        <NavItem
+          preventDefault
+          to={URI_DOMAIN}
+          itemId="domain"
+          isActive={activeItem === URI_DOMAIN}
+        >
+          Domain
+        </NavItem>
+      </NavGroup>
+
+      <NavGroup title="User access">
+        <NavItem
+          preventDefault
+          to={URI_CREDENTIALS}
+          itemId="credentials"
+          isActive={activeItem === URI_CREDENTIALS}
+        >
+          Username & password
+        </NavItem>
+        <NavItem
+          preventDefault
+          to={URI_SSHKEY}
+          itemId="sshkey"
+          isActive={activeItem === URI_SSHKEY}
+        >
+          SSH key
+        </NavItem>
+      </NavGroup>
+
+      <NavGroup title="Console">
+        <NavItem
+          preventDefault
+          to={URI_CONSOLE}
+          itemId="console"
+          isActive={activeItem === URI_CONSOLE}
+        >
+          OpenShift console
         </NavItem>
       </NavGroup>
     </Nav>
