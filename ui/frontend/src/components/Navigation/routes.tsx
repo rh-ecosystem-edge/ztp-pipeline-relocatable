832401bc4714c2eef98afea003fa6382034c1448
Marek Libra <marek.libra@gmail.com>
2022-10-18 13:29:30 +0200

    Stub all pages

---
+++ ui/frontend/src/components/Navigation/routes.tsx
@@ -0,0 +1,7 @@
+export const URI_LAYER3 = '/layer3';
+export const URI_INGRESS = '/ingressip';
+export const URI_API = '/apiip';
+export const URI_DOMAIN = '/domain';
+export const URI_CREDENTIALS = '/credentials';
+export const URI_SSHKEY = '/sshkey';
+export const URI_CONSOLE = '/console';
