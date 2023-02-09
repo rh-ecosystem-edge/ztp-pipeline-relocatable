5d955e17dfe27ced7dc1f0a8c4cbdd5c6149e4af
Juan Hernández <juan.hernandez@redhat.com>
2023-02-09 11:55:59 +0100

    NO-ISSUE: Fix calculation of DNS domain (#704)
    The DNS domain is currently copied from the configuration of the
    ingressc controller of the hub. For example, if that configuration is
    `apps.my-hub.my-domain.com` then the base DNS domain of the edge
    clusters will also be `apps.my-hub.my-domain.com`. But that is wrong; it
    should be only `my-domain.com`. This patch fixes that.
    
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- ztp/internal/cmd/create_cluster_cmd_test.go
+++ ztp/internal/cmd/create_cluster_cmd_test.go
@@ -93,7 +93,7 @@ var _ = Describe("Create cluster command", func() {
 					"auths": {
 						"cloud.openshift.com": {
 							"auth": "bXktdXNlcjpteS1wYXNz",
-							"email": "mary@example.com"
+							"email": "mary@my-domain.com"
 						}
 					}
 				}`),
