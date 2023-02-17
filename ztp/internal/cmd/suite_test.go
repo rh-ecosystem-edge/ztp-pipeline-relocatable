03cf0c01b2406412d84d1140d0bb6476860082a6
Juan Hernandez <juan.hernandez@redhat.com>
2023-02-17 12:48:16 +0100

    NO-ISSUE: Avoid log package interferences with tests
    This patch changes the tests so that it redirect the `log` package to
    the `GinkgoWriter` to avoid interferences of logs generated by the http
    package.
    
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- ztp/internal/cmd/suite_test.go
+++ ztp/internal/cmd/suite_test.go
@@ -15,6 +15,7 @@ License.
 package cmd
 
 import (
+	"log"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2/dsl/core"
@@ -25,3 +26,7 @@ func TestCmd(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Commands")
 }
+
+var _ = BeforeSuite(func() {
+	log.SetOutput(GinkgoWriter)
+})
--- ztp/internal/suite_test.go
+++ ztp/internal/suite_test.go
@@ -15,6 +15,7 @@ License.
 package internal
 
 import (
+	"log"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2/dsl/core"
@@ -25,3 +26,7 @@ func TestInternal(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Internal")
 }
+
+var _ = BeforeSuite(func() {
+	log.SetOutput(GinkgoWriter)
+})
